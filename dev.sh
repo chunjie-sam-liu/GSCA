84f3278a627775aee7ae608bacb1c1c41fca9300
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-09-02 17:06:42 +0800

    backend dev env

---
+++ dev.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: 2020-09-02 16:21:48
+# @DESCRIPTION:
+
+git pull
+
+source venv/bin/activate
+pip install -r requirements.txt
