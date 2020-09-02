521074c8ef1696764cf1ba79173fca341e629e7c
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-09-02 17:30:27 +0800

    backend done

--- gsca/routes/home.py
+++ gsca/routes/home.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from gsca.db import mongo
-from flask_restful import Api
+from flask_restful import Api, Resource
 
 
 home = Blueprint("home", __name__)
@@ -8,6 +8,11 @@ home = Blueprint("home", __name__)
 api = Api(home)
 
 
-@home.route("/", methods=["GET"])
-def index():
-    return render_template("index.html")
+class TestGSCA(Resource):
+    def get(self):
+        print("cj")
+        return {"new": "gsca"}
+
+
+api.add_resource(TestGSCA, "/gsca")
+
