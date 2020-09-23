36396c1db375cf082423fc6d080a9769df1220fd
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-09-23 13:29:41 +0800

    app module

--- gsca/gsca-angular/src/app/app.component.ts
+++ gsca/gsca-angular/src/app/app.component.ts
@@ -6,7 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'gsca-angular';
-  public cj = 'harry porter';
-  public backendData = 'new: gsca';
+  title = 'GSCA';
 }
--- gsca/gsca-angular/src/app/app.module.ts
+++ gsca/gsca-angular/src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
