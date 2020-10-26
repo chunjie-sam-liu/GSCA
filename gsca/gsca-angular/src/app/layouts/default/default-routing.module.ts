2ea40bbf2f02dac1636b9b92f5e9cf287cff2c58
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-10-26 17:09:18 +0800

    default routing

--- gsca/gsca-angular/src/app/layouts/default/default-routing.module.ts
+++ gsca/gsca-angular/src/app/layouts/default/default-routing.module.ts
@@ -13,6 +13,22 @@ const routes: Routes = [
         loadChildren: () => import('src/app/modules/dashboard/dashboard.module').then((m) => m.DashboardModule),
       },
       {
+        path: 'expression',
+        loadChildren: () => import('src/app/modules/expression/expression.module').then((m) => m.ExpressionModule),
+      },
+      {
+        path: 'mutation',
+        loadChildren: () => import('src/app/modules/mutation/mutation.module').then((m) => m.MutationModule),
+      },
+      {
+        path: 'immune',
+        loadChildren: () => import('src/app/modules/immune/immune.module').then((m) => m.ImmuneModule),
+      },
+      {
+        path: 'drug',
+        loadChildren: () => import('src/app/modules/drug/drug.module').then((m) => m.DrugModule),
+      },
+      {
         path: 'document',
         loadChildren: () => import('src/app/modules/document/document.module').then((m) => m.DocumentModule),
       },
