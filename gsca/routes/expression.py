5be6e217283263b6ac22df38beacbe3196888275
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-10-26 20:42:40 +0800

    test routes

---
+++ gsca/routes/expression.py
@@ -0,0 +1,29 @@
+from flask import Blueprint
+from gsca.db import mongo
+from flask_restful import Api, Resource, fields, marshal_with, reqparse
+
+
+expression = Blueprint("expression", __name__)
+api = Api(expression)
+
+mdoel_degs = {}
+
+
+class DEG(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("symbol", type=str, required=True)
+        parser.add_argument("cancer_type", type=str)
+
+        args = parser.parse_args()
+
+        condition = {}
+        if args.filter != "":
+            condition["symbol"] = {"$regex": args.filter, "$options": "i"}
+        mcur = mongo.db["BLCA_deg"].find(condition)
+        n_record = mcur.count()
+        return {""}
+
+
+api.add_resource(DEG, "/degs")
+
--- gsca/routing.py
+++ gsca/routing.py
@@ -1,11 +1,13 @@
 from flask import render_template
 from gsca import app
 
-from gsca.routes.home import home
+# from gsca.routes.home import home
+from gsca.routes.expression import expression
 
 
 # routing
-app.register_blueprint(home, url_prefix="/api/home")
+# app.register_blueprint(home, url_prefix="/api/home")
+app.register_blueprint(expression, url_prefix="/api/expression")
 
 
 @app.route("/", methods=["GET"])
