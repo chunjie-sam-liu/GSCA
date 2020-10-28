fb7e28d9ce06602cd4bc2307b263a97aadf0fb13
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-10-28 16:17:52 +0800

    expression

--- gsca/gsca-angular/src/app/modules/expression/expression.component.ts
+++ gsca/gsca-angular/src/app/modules/expression/expression.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ExprSearch } from 'src/app/shared/model/exprsearch';
 
 @Component({
   selector: 'app-expression',
   templateUrl: './expression.component.html',
-  styleUrls: ['./expression.component.css']
+  styleUrls: ['./expression.component.css'],
 })
 export class ExpressionComponent implements OnInit {
+  searchSymbol: string[];
+  selectedCancerTypes: string[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public showContent(exprSearch: ExprSearch): void {
+    this.searchSymbol = exprSearch.validSymbol;
+    this.selectedCancerTypes = exprSearch.cancerTypesSelected;
   }
-
 }
