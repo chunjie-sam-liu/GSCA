22d5b956a50da7bae058657608f613ded5792b71
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-10-29 17:48:21 +0800

    front end sort and pagination and filter

--- gsca/gsca-angular/src/app/modules/expression/deg/deg.component.ts
+++ gsca/gsca-angular/src/app/modules/expression/deg/deg.component.ts
@@ -1,15 +1,25 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { ExprSearch } from 'src/app/shared/model/exprsearch';
 import { ExpressionApiService } from '../expression-api.service';
 import collectionList from 'src/app/shared/constants/collectionlist';
+import { MatTableDataSource } from '@angular/material/table';
+import { DegTableRecord } from 'src/app/shared/model/degtablerecord';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-deg',
   templateUrl: './deg.component.html',
   styleUrls: ['./deg.component.css'],
 })
-export class DegComponent implements OnInit, OnChanges {
+export class DegComponent implements OnInit, OnChanges, AfterViewInit {
   @Input() searchTerm: ExprSearch;
+
+  // deg table data source
+  dataSourceDeg: MatTableDataSource<DegTableRecord>;
+  @ViewChild('paginatorDeg', { static: true }) paginatorDeg: MatPaginator;
+  @ViewChild(MatSort) sortDeg: MatSort;
+  displayedColumnsDeg = ['cancertype', 'symbol', 'tumor', 'normal', 'fc', 'fdr', 'n_tumor'];
 
   constructor(private expressionApiService: ExpressionApiService) {}
 
@@ -17,11 +27,15 @@ export class DegComponent implements OnInit, OnChanges {
 
   ngOnChanges(changes: SimpleChanges): void {
     const postTerm = this._validCollection(this.searchTerm);
-    console.log(postTerm);
+
     this.expressionApiService.getDEGTable(postTerm).subscribe((res) => {
-      console.log(res);
+      this.dataSourceDeg = new MatTableDataSource(res);
+      this.dataSourceDeg.paginator = this.paginatorDeg;
+      this.dataSourceDeg.sort = this.sortDeg;
     });
   }
+
+  ngAfterViewInit(): void {}
 
   private _validCollection(st: ExprSearch): any {
     st.validColl = st.cancerTypeSelected
@@ -31,5 +45,13 @@ export class DegComponent implements OnInit, OnChanges {
       .filter(Boolean);
 
     return st;
+  }
+  public applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSourceDeg.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSourceDeg.paginator) {
+      this.dataSourceDeg.paginator.firstPage();
+    }
   }
 }
