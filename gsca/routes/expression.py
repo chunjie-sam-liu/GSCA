7024478c29eee3c47bbd8e59c0b8f7184d73de19
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-10-29 21:23:50 +0800

    path issue

--- gsca/routes/expression.py
+++ gsca/routes/expression.py
@@ -10,10 +10,10 @@ expression = Blueprint("expression", __name__)
 api = Api(expression)
 
 # r plot resource
-apppath = Path(api.app.root_path)
+apppath = Path(api.app.root_path).parent.parent
 rcommand = "/usr/bin/Rscript"
-rscriptpath = apppath.parent / "rscripts"
-resource_pngs = apppath.parent / "resource/pngs"
+rscriptpath = apppath / "gsca/rscripts"
+resource_pngs = apppath / "gsca/resource/pngs"
 
 if not resource_pngs.exists():
     resource_pngs.mkdir(parents=True)
@@ -58,7 +58,7 @@ class DEGplot(Resource):
         filename = rarg + ".png"
         filepath = resource_pngs / filename
 
-        cmd = [rcommand, str(rscriptpath / "degplot.R"), rarg, str(filepath)]
+        cmd = [rcommand, str(rscriptpath / "degplot.R"), rarg, str(filepath), str(apppath)]
         if not filepath.exists():
             subprocess.check_output(cmd, universal_newlines=True)
 
--- gsca/rscripts/degplot.R
+++ gsca/rscripts/degplot.R
@@ -11,9 +11,11 @@ args <- commandArgs(TRUE)
 
 search_str <- args[1]
 filepath <- args[2]
+apppath <- args[3]
 
 # search_str = 'A2M#ACE#ANGPT2#BPI#CD1B#CDR1#EGR2#EGR3#HBEGF#HERPUD1#MCM2#PCTP#PODXL#PPY#PTGS2#RCAN1#SLC4A7#THBD@KICH_deg#KIRC_deg#KIRP_deg#LUAD_deg#LUSC_deg'
 # filepath = '/home/liucj/github/GSCA/gsca/resource/pngs/A2M#ACE#ANGPT2#BPI#CD1B#CDR1#EGR2#EGR3#HBEGF#HERPUD1#MCM2#PCTP#PODXL#PPY#PTGS2#RCAN1#SLC4A7#THBD@KICH_deg#KIRC_deg#KIRP_deg#LUAD_deg#LUSC_deg.png'
+apppath <- '/home/liucj/github/GSCA'
 
 search_str_split <- strsplit(x = search_str, split = '@')[[1]]
 search_genes <- strsplit(x = search_str_split[1], split = '#')[[1]]
@@ -28,4 +30,4 @@ search_cancertypes <- strsplit(x = search_str_split[[2]], split = '#')[[1]]
 print(search_str)
 print(filepath)
 
-readr::write_tsv(tibble::tibble(search_str = search_str, here = here::here('gsca/rscripts/gsca.conf')), file = filepath)
+readr::write_tsv(tibble::tibble(search_str = search_str, here = file.path(apppath, 'gsca/rscripts/gsca.conf')), file = filepath)
