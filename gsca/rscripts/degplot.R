469fd599088aee6a50b799a6d0874cf4efd1cc39
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-10-29 21:26:41 +0800

    deg

--- gsca/rscripts/degplot.R
+++ gsca/rscripts/degplot.R
@@ -15,7 +15,7 @@ apppath <- args[3]
 
 # search_str = 'A2M#ACE#ANGPT2#BPI#CD1B#CDR1#EGR2#EGR3#HBEGF#HERPUD1#MCM2#PCTP#PODXL#PPY#PTGS2#RCAN1#SLC4A7#THBD@KICH_deg#KIRC_deg#KIRP_deg#LUAD_deg#LUSC_deg'
 # filepath = '/home/liucj/github/GSCA/gsca/resource/pngs/A2M#ACE#ANGPT2#BPI#CD1B#CDR1#EGR2#EGR3#HBEGF#HERPUD1#MCM2#PCTP#PODXL#PPY#PTGS2#RCAN1#SLC4A7#THBD@KICH_deg#KIRC_deg#KIRP_deg#LUAD_deg#LUSC_deg.png'
-apppath <- '/home/liucj/github/GSCA'
+# apppath <- '/home/liucj/github/GSCA'
 
 search_str_split <- strsplit(x = search_str, split = '@')[[1]]
 search_genes <- strsplit(x = search_str_split[1], split = '#')[[1]]
