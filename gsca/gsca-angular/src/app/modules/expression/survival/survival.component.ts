1e5fa97dd30e8f37ff05ae2cf11f9be9c39cfadb
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-11-03 17:05:42 +0800

    rm console

--- gsca/gsca-angular/src/app/modules/expression/survival/survival.component.ts
+++ gsca/gsca-angular/src/app/modules/expression/survival/survival.component.ts
@@ -36,7 +36,6 @@ export class SurvivalComponent implements OnInit, OnChanges, AfterViewInit {
     this.dataSourceSurvivalLoading = true;
     this.survivalImageLoading = true;
     const postTerm = this._validCollection(this.searchTerm);
-    console.log(postTerm);
 
     this.expressionApiService.getSurvivalTable(postTerm).subscribe((res) => {
       this.dataSourceSurvivalLoading = false;
