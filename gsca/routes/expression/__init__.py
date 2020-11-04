3e9172c250875185ea279fe3c151af9c1f583249
Huffyphenix <949985347@qq.com>
2020-11-04 23:34:35 +0800

    add subtype to init config

--- gsca/routes/expression/__init__.py
+++ gsca/routes/expression/__init__.py
@@ -1,6 +1,8 @@
 from gsca import app
 from gsca.routes.expression.deg import deg
 from gsca.routes.expression.survival import survival
+from gsca.routes.expression.subtype import subtype
 
 app.register_blueprint(deg, url_prefix="/api/expression/deg")
 app.register_blueprint(survival, url_prefix="/api/expression/survival")
+app.register_blueprint(subtype, url_prefix="/api/expression/subtype")
