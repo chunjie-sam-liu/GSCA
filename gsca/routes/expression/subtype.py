24a4940dd25fff58bd3deee3afcbfce935319662
Huffyphenix <949985347@qq.com>
2020-11-05 10:47:39 +0800

    subtype backend

--- gsca/routes/expression/subtype.py
+++ gsca/routes/expression/subtype.py
@@ -18,55 +18,12 @@ if not resource_pngs.exists():
     resource_pngs.mkdir(parents=True)
 
 model_subtypetable = {
+    "entrez": fields.Integer(attribute="entrez"),
     "symbol": fields.String(attribute="symbol"),
     "pval": fields.Float(attribute="pval"),
     "fdr": fields.Float(attribute="fdr"),
     "cancertype": fields.String(attribute="cancertype"),
 }
-
-
-class SubtypePlot(Resource):
-    def post(self):
-        args = request.get_json()
-        res = self.__check_run(args)
-
-        if res["run"]:
-            self.__subtypeplot(args, res["filepath"])
-        return send_file((str(res["filepath"])), mimetype="image/png")
-
-    def __check_run(self, args):
-        uuidname = str(uuid.uuid4())
-        filename = uuidname + ".png"
-        filepath = resource_pngs / filename
-
-        preanalysised = mongo.db.preanalysised.find_one(
-            {"search": "#".join(args["validSymbol"]), "coll": "#".join(args["validColl"]), "purpose": "subtypeplot"},
-            {"_id": 0, "uuid": 1},
-        )
-        print(preanalysised)
-
-        if preanalysised:
-            uuidname = preanalysised["uuid"]
-            filename = preanalysised["filename"]
-            filepath = resource_pngs / filename
-            run = False if filepath.exists() else True
-            return {"run": run, "filepath": filepath}
-        else:
-            mongo.db.preanalysised.insert_one(
-                {
-                    "search": "#".join(args["validSymbol"]),
-                    "coll": "#".join(args["validColl"]),
-                    "purpose": "survivalplot",
-                    "uuid": uuidname,
-                }
-            )
-
-    def __subtypeplot(self, args, filepath):
-        rargs = "#".join(args["validSymbol"]) + "@" + "#".join(args["validColl"])
-
-        cmd = [rcommand, str(rscriptpath / "exp_subtypeplot_profile.R"), rargs, str(filepath), str(apppath)]
-        print(cmd)
-        subprocess.check_output(cmd, universal_newlines=True)
 
 
 class SubtypeTable(Resource):
@@ -86,3 +43,49 @@ class SubtypeTable(Resource):
 
 api.add_resource(SubtypeTable, "/subtypetable")
 
+
+class SubtypePlot(Resource):
+    def post(self):
+        args = request.get_json()
+        res = self.__check_run(args)
+
+        if res["run"]:
+            self.__subtypeplot(args, res["filepath"])
+        return send_file(str(res["filepath"]), mimetype="image/png")
+
+    def __check_run(self, args):
+        uuidname = str(uuid.uuid4())
+        filename = uuidname + ".png"
+        filepath = resource_pngs / filename
+
+        preanalysised = mongo.db.preanalysised.find_one(
+            {"search": "#".join(args["validSymbol"]), "coll": "#".join(args["validColl"]), "purpose": "subtypeplot"},
+            {"_id": 0, "uuid": 1},
+        )
+
+        if preanalysised:
+            uuidname = preanalysised["uuid"]
+            filename = uuidname + ".png"
+            filepath = resource_pngs / filename
+            run = False if filepath.exists() else True
+            return {"run": run, "filepath": filepath}
+        else:
+            mongo.db.preanalysised.insert_one(
+                {
+                    "search": "#".join(args["validSymbol"]),
+                    "coll": "#".join(args["validColl"]),
+                    "purpose": "survivalplot",
+                    "uuid": uuidname,
+                }
+            )
+            return {"run": True, "filepath": filepath}
+
+    def __subtypeplot(self, args, filepath):
+        rargs = "#".join(args["validSymbol"]) + "@" + "#".join(args["validColl"])
+
+        cmd = [rcommand, str(rscriptpath / "exp_subtypeplot_profile.R"), rargs, str(filepath), str(apppath)]
+        print(cmd)
+        subprocess.check_output(cmd, universal_newlines=True)
+
+
+api.add_resource(SubtypePlot, "/subtypeplot")
