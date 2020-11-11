bb0610bd8f88c0d5d4e364c99cfa30c8b452a47a
Huffyphenix <949985347@qq.com>
2020-11-11 21:13:17 +0800

    subtype single gene

--- gsca/routes/expression/subtype.py
+++ gsca/routes/expression/subtype.py
@@ -45,3 +45,29 @@ class SubtypePlot(Resource):
 
 
 api.add_resource(SubtypePlot, "/subtypeplot")
+
+
+class SubtypePlotSingleGene(Resource):
+    def post(self):
+        args = request.get_json()
+        checkplot = CheckPlot(args=args, purpose="subtypeplotsinglegene", rplot="subtypeplot_singlegene.R")
+        res = checkplot.check_run()
+        if res["run"]:
+            checkplot.plot(filepath=res["filepath"])
+        return send_file(str(res["filepath"]), mimetype="image/png")
+
+
+api.add_resource(SubtypePlotSingleGene, "/single/gene")
+
+
+class SubtypePlotSingleCancerType(Resource):
+    def post(self):
+        args = request.get_json()
+        checkplot = CheckPlot(args=args, purpose="subtypeplotsinglecancertype", rplot="subtypeplot_singlecancertype.R")
+        res = checkplot.check_run()
+        if res["run"]:
+            checkplot.plot(filepath=res["filepath"])
+        return send_file(str(res["filepath"]), mimetype="image/png")
+
+
+api.add_resource(SubtypePlotSingleCancerType, "/single/cancertype")
