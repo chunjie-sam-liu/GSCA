dbc2fb8529d538090412e4720f43ed9429078d19
Huffyphenix <949985347@qq.com>
2020-11-16 17:43:11 +0800

    generate snv component in mutation module

---
+++ gsca-angular/src/app/modules/mutation/snv/snv.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-snv',
+  templateUrl: './snv.component.html',
+  styleUrls: ['./snv.component.css']
+})
+export class SnvComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
