d6fd84cb2fd4f15fe6b59b280a6ade66f7d1834f
Huffyphenix <949985347@qq.com>
2020-11-16 21:59:31 +0800

    snv api

--- gsca-angular/src/app/modules/mutation/mutation-api.service.ts
+++ gsca-angular/src/app/modules/mutation/mutation-api.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BaseHttpService } from 'src/app/shared/base-http.service';
+import { ExprSearch } from 'src/app/shared/model/exprsearch';
 
 @Injectable({
   providedIn: 'root',
@@ -9,5 +10,15 @@ import { BaseHttpService } from 'src/app/shared/base-http.service';
 export class MutationApiService extends BaseHttpService {
   constructor(http: HttpClient) {
     super(http);
+  }
+
+  public getSnvTable(postTerm: ExprSearch): Observable<any> {
+    return this.postData('mutation/snv/snvtable', postTerm);
+  }
+  public getSnvPlot(postTerm: ExprSearch): Observable<any> {
+    return this.postDataImage('mutation/snv/snvplot', postTerm);
+  }
+  public getSnvSingleGenePlot(postTerm: ExprSearch): Observable<any> {
+    return this.postDataImage('mutation/snv/snvsinglegeneplot', postTerm);
   }
 }
