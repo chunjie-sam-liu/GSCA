b5e709b3fc03305788379bcb23849f6a3be8befd
Huffyphenix <949985347@qq.com>
2020-11-16 17:42:23 +0800

    immune search box

--- gsca-angular/src/app/modules/immune/immune.module.ts
+++ gsca-angular/src/app/modules/immune/immune.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ImmuneRoutingModule } from './immune-routing.module';
 import { ImmuneComponent } from './immune.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { SearchBoxComponent } from './search-box/search-box.component';
 
 @NgModule({
-  declarations: [ImmuneComponent],
+  declarations: [ImmuneComponent, SearchBoxComponent],
   imports: [CommonModule, ImmuneRoutingModule, SharedModule],
 })
 export class ImmuneModule {}
