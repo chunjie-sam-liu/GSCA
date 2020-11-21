a0965978dcf92d29ee3a419241bb97b4c02bdb21
Huffyphenix <949985347@qq.com>
2020-11-21 12:50:17 +0800

    new snv survival mongo

--- data/src/13-snv-survival.R
+++ data/src/13-snv-survival.R
@@ -14,18 +14,25 @@ data_path <- "/home/huff/data/GSCA/snv"
 gsca_conf <- readr::read_lines(file = file.path(rda_path,"src",'gsca.conf'))
 
 # Load snv survival----------------------------------------------------------------
-
-snv_survival <- readr::read_rds(file.path(data_path,"pancan32_snv_survival_genelist_sig_pval.IdTrans.rds.gz"))
-
+file_list <- grep("*_survival.snv.rds.gz",dir(file.path(data_path,"cancer_snv_survival")),value = TRUE)
+snv_survival <- tibble::tibble()
+for (file in file_list) {
+  .snv_survival <- readr::read_rds(file.path(data_path,"cancer_snv_survival",file)) %>%
+    dplyr::mutate(cancer_types= strsplit(file,split = "_")[[1]][1])
+  if(nrow(snv_survival)<1){
+    snv_survival<-.snv_survival
+  } else {
+    rbind(snv_survival,.snv_survival) ->snv_survival
+  }
+}
 
 # Function ----------------------------------------------------------------
 
-fn_snv_survival_mongo <-function(cancer_types,snv_survival){
+fn_snv_survival_mongo <-function(cancer_types,data){
   .y <- cancer_types 
-  .x <- snv_survival %>%
-    dplyr::rename(higher_risk_of_death=worse,cox_p=coxP,log_rank_p=logRankP) %>%
-    dplyr::mutate(higher_risk_of_death=ifelse(higher_risk_of_death=="High","Mutated","Non-mutated")) %>%
-    dplyr::mutate(HR = exp(estimate))
+  .x <- data %>%
+    dplyr::rename(symbol=Hugo_Symbol ,log_rank_p=logrankp,HR=hr) %>%
+    dplyr::mutate(higher_risk_of_death=ifelse(higher_risk_of_death=="High","Mutated","Non-mutated"))
   
   # insert to collection
   .coll_name <- glue::glue('{.y}_snv_survival')
@@ -42,6 +49,8 @@ fn_snv_survival_mongo <-function(cancer_types,snv_survival){
 
 # data --------------------------------------------------------------------
 snv_survival %>%
+  dplyr::filter(!is.na(higher_risk_of_death)) %>%
+  tidyr::nest(data = c(Hugo_Symbol, entrez, logrankp, cox_p, hr, higher_risk_of_death, sur_type)) %>%
   purrr::pmap(.f=fn_snv_survival_mongo) -> snv_survival_mongo_data
 
 # Save image --------------------------------------------------------------
