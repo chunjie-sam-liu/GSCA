025b19a6f87c9c1cda9fe2735e57c1e554304a5c
Huffyphenix <949985347@qq.com>
2020-11-25 21:06:47 +0800

    methy backend

--- gsca/routes/mutation/__init__.py
+++ gsca/routes/mutation/__init__.py
@@ -1,7 +1,9 @@
 from gsca import app
 from gsca.routes.mutation.snv import snv
 from gsca.routes.mutation.snvsurvival import snvsurvival
+from gsca.routes.mutation.methylation import methylation
 
 
 app.register_blueprint(snv, url_prefix="/api/mutation/snv")
 app.register_blueprint(snvsurvival, url_prefix="/api/mutation/snvsurvival")
+app.register_blueprint(methylation, url_prefix="/api/mutation/methylation")
