fb55b8c015d2d8243a1f40993fe65879c8b98bdf
Huffyphenix <949985347@qq.com>
2020-11-27 20:39:15 +0800

    methy diff used utils to fetch

--- gsca-r-app/methy_diff_plot.R
+++ gsca-r-app/methy_diff_plot.R
@@ -26,93 +26,25 @@ search_colls <- strsplit(x = search_str_split[[2]], split = '#')[[1]]
 source(file.path(apppath, "gsca-r-app/utils/fn_figure_height.R"))
 size <- fn_height_width(search_genes,search_colls)
 
-# Mongo -------------------------------------------------------------------
+# fetch data --------------------------------------------------------------
 
-gsca_conf <- readr::read_lines(file = file.path(apppath, 'gsca-r-app/gsca.conf'))
-
-# Function ----------------------------------------------------------------
-
-fn_query_str <- function(.x) {
-  .xx <- paste0(.x, collapse = '","')
-  glue::glue('{"symbol": {"$in": ["<.xx>"]}}', .open = '<', .close = '>')
-}
-
-fn_fetch_mongo <- function(.x) {
-  .coll <- mongolite::mongo(collection = .x, url = gsca_conf)
-  .coll$find(
-    query = fn_query_str(search_genes),
-    fields = '{"symbol": true, "fc": true,"trend": true,"gene_tag": true,"logfdr": true, "_id": false}'
-  ) %>%
-    dplyr::mutate(cancertype = gsub(pattern = '_methy_diff', replacement = '', x = .x))
-}
-
-fn_filter_pattern <- function(trend,logfdr) {
-  if ((trend == "Up") && (logfdr > 1.3)) {
-    return(1)
-  } else if ((trend == "Down") && (logfdr >1.3)) {
-    return(-1)
-  } else {
-    return(0)
-  }
-}
-
-fn_get_pattern <- function(.x) {
-  .x %>%
-    dplyr::mutate(pattern = purrr::map2_dbl(trend,logfdr, fn_filter_pattern)) %>%
-    dplyr::select(cancertype, symbol, pattern ) %>%
-    tidyr::spread(key = cancertype, value = pattern) %>%
-    dplyr::mutate_if(.predicate = is.numeric, .funs = function(.) {ifelse(is.na(.), 0, .)})
-}
-
-fn_get_cancer_types_rank <- function(.x) {
-  .x %>%
-    dplyr::summarise_if(.predicate = is.numeric, dplyr::funs(sum(abs(.)))) %>%
-    tidyr::gather(key = cancertype, value = rank) %>%
-    dplyr::arrange(dplyr::desc(rank))
-}
-
-fn_get_gene_rank <- function(.x) {
-  .x %>%
-    dplyr::rowwise() %>%
-    dplyr::do(
-      symbol = .$symbol,
-      rank = unlist(.[-1][-1], use.names = F) %>% sum(),
-      high = (unlist(.[-1][-1], use.names = F) == 1) %>% sum(),
-      low = (unlist(.[-1][-1], use.names = F) == -1) %>% sum()
-    ) %>%
-    dplyr::ungroup() %>%
-    tidyr::unnest(cols = c(symbol, rank, high, low)) %>%
-    dplyr::arrange(rank)
-}
-
-fn_pval_class <- function(.p){
-  if(.p>0.05){
-    ""
-  }else if(.p<=0.05 & .p>=0.01){
-    "*"
-  }else if(.p<0.01 & .p>=0.001){
-    "**"
-  } else{
-    "***"
-  }
-}
-fn_pval_label <- function(.x){
-  .x %>%
-    dplyr::mutate(p_label=purrr::map(log_rank_p,fn_pval_class)) %>%
-    tidyr::unnest()
-}
-
-# Query data --------------------------------------------------------------
-
-fetched_data <- purrr::map(.x = search_colls, .f = fn_fetch_mongo) %>% dplyr::bind_rows()
+source(file.path(apppath, "gsca-r-app/utils/fn_fetch_mongo_data.R"))
+fields <- '{"symbol": true, "fc": true,"trend": true,"gene_tag": true,"logfdr": true, "_id": false}'
+fetched_data <- purrr::map(.x = search_colls, .f = fn_fetch_mongo, pattern="_methy_diff",fields = fields,.key=search_genes,.keyindex="symbol") %>%
+  dplyr::bind_rows()
 fetched_data %>%
   dplyr::mutate(tag = purrr::map(gene_tag,.f=function(.x){strsplit(.x,"_")[[1]][1]})) %>%
   dplyr::select(-gene_tag) %>%
   tidyr::unnest(tag) -> fetched_data
 
-# Sort --------------------------------------------------------------------
+# Sort ----------------------------------------------------------------
+source(file.path(apppath,"gsca-r-app/utils/common_used_summary_plot_functions.R"))
 
-fetched_data_clean_pattern <- fn_get_pattern(.x = fetched_data)
+fetched_data_clean_pattern <- fn_get_pattern(.x = fetched_data %>% dplyr::mutate(value=10^(-logfdr)),
+                                             trend1="Up",
+                                             trend2="Down",
+                                             p_cutoff=1.3,
+                                             selections = c("cancertype","sur_type"))
 cancer_rank <- fn_get_cancer_types_rank(.x = fetched_data_clean_pattern)
 gene_rank <- fn_get_gene_rank(.x = fetched_data_clean_pattern)
 
