c818bb887ac88682d1a140378f9e4c6e562c9302
Huffyphenix <949985347@qq.com>
2020-12-01 16:07:00 +0800

    cnv survival backend

--- gsca/routes/mutation/__init__.py
+++ gsca/routes/mutation/__init__.py
@@ -6,6 +6,7 @@ from gsca.routes.mutation.methysurvival import methysurvival
 from gsca.routes.mutation.methycor import methycor
 from gsca.routes.mutation.cnv import cnv
 from gsca.routes.mutation.cnvcor import cnvcor
+from gsca.routes.mutation.cnvsurvival import cnvsurvival
 
 app.register_blueprint(snv, url_prefix="/api/mutation/snv")
 app.register_blueprint(snvsurvival, url_prefix="/api/mutation/snvsurvival")
@@ -14,3 +15,4 @@ app.register_blueprint(methysurvival, url_prefix="/api/mutation/methysurvival")
 app.register_blueprint(methycor, url_prefix="/api/mutation/methycor")
 app.register_blueprint(cnv, url_prefix="/api/mutation/cnv")
 app.register_blueprint(cnvcor, url_prefix="/api/mutation/cnvcor")
+app.register_blueprint(cnvsurvival, url_prefix="/api/mutation/cnvsurvival")
