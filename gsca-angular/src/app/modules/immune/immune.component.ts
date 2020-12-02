db153a129897f160f7eb05c92ec8564c2351ff98
Huffyphenix <949985347@qq.com>
2020-12-02 10:38:16 +0800

    immune config

--- gsca-angular/src/app/modules/immune/immune.component.ts
+++ gsca-angular/src/app/modules/immune/immune.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ExprSearch } from 'src/app/shared/model/exprsearch';
 
 @Component({
   selector: 'app-immune',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./immune.component.css']
 })
 export class ImmuneComponent implements OnInit {
-
+  searchTerm: ExprSearch;
+  showImmExpr = false;
+  showImmSnv = false;
+  showImmCnv = false;
+  showImmMethy = false;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  ngAfterViewInit(): void {}
+  
+  public showContent(exprSearch: ExprSearch): void {
+    this.searchTerm = exprSearch;
+    this.showImmExpr = true;
+    this.showImmSnv = true;
+    this.showImmCnv = true;
+    this.showImmMethy = true;
   }
-
 }
