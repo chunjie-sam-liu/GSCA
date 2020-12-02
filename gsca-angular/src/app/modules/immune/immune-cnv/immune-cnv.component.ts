97317f60b11cb46369e294059ae29bef603d5637
Huffyphenix <949985347@qq.com>
2020-12-02 10:20:51 +0800

    generate immune-cnv

---
+++ gsca-angular/src/app/modules/immune/immune-cnv/immune-cnv.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-immune-cnv',
+  templateUrl: './immune-cnv.component.html',
+  styleUrls: ['./immune-cnv.component.css']
+})
+export class ImmuneCnvComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- gsca-angular/src/app/modules/immune/immune.module.ts
+++ gsca-angular/src/app/modules/immune/immune.module.ts
@@ -6,9 +6,10 @@ import { ImmuneComponent } from './immune.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { SearchBoxComponent } from './search-box/search-box.component';
 import { ImmuneExprComponent } from './immune-expr/immune-expr.component';
+import { ImmuneCnvComponent } from './immune-cnv/immune-cnv.component';
 
 @NgModule({
-  declarations: [ImmuneComponent, SearchBoxComponent, ImmuneExprComponent],
+  declarations: [ImmuneComponent, SearchBoxComponent, ImmuneExprComponent, ImmuneCnvComponent],
   imports: [CommonModule, ImmuneRoutingModule, SharedModule],
 })
 export class ImmuneModule {}
