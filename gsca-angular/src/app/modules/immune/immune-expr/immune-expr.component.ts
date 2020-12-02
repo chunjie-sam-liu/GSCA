82a4b983d16a9598af3ccb88d7b9e8a0c3ecaccf
Huffyphenix <949985347@qq.com>
2020-12-02 10:20:06 +0800

    generate immune-expr

---
+++ gsca-angular/src/app/modules/immune/immune-expr/immune-expr.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-immune-expr',
+  templateUrl: './immune-expr.component.html',
+  styleUrls: ['./immune-expr.component.css']
+})
+export class ImmuneExprComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- gsca-angular/src/app/modules/immune/immune.module.ts
+++ gsca-angular/src/app/modules/immune/immune.module.ts
@@ -5,9 +5,10 @@ import { ImmuneRoutingModule } from './immune-routing.module';
 import { ImmuneComponent } from './immune.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { SearchBoxComponent } from './search-box/search-box.component';
+import { ImmuneExprComponent } from './immune-expr/immune-expr.component';
 
 @NgModule({
-  declarations: [ImmuneComponent, SearchBoxComponent],
+  declarations: [ImmuneComponent, SearchBoxComponent, ImmuneExprComponent],
   imports: [CommonModule, ImmuneRoutingModule, SharedModule],
 })
 export class ImmuneModule {}
