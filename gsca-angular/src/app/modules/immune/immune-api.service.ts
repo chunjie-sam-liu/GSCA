14a6b8f509e5a1513f6dc8fbadd30fcffd340ae8
Huffyphenix <949985347@qq.com>
2020-12-02 14:43:36 +0800

    imm cor cnv fronend v1

--- gsca-angular/src/app/modules/immune/immune-api.service.ts
+++ gsca-angular/src/app/modules/immune/immune-api.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BaseHttpService } from 'src/app/shared/base-http.service';
+import { ExprSearch } from 'src/app/shared/model/exprsearch';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ImmuneApiService {
+export class ImmuneApiService extends BaseHttpService {
 
-  constructor() { }
+  constructor(http: HttpClient) { 
+    super(http);
+  }
+
+  public getImmCnvCorTable(postTerm: ExprSearch): Observable<any> {
+    return this.postData('immune/immunecnv/immunecnvcortable', postTerm);
+  }
+  public getImmCnvCorPlot(postTerm: ExprSearch): Observable<any> {
+    return this.postDataImage('immune/immunecnv/immunecnvcorplot', postTerm);
+  }
+  public getImmCnvCorSingleGene(postTerm: ExprSearch): Observable<any> {
+    return this.postDataImage('immune/immunecnv/immunecnvcorsinglegene', postTerm);
+  }
+
 }
