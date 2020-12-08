d35e285014b782e8536a9a94e4b53ad5aa75d15c
Huffyphenix <949985347@qq.com>
2020-12-08 16:22:01 +0800

    rscript immune cor plot optimize

--- gsca-r-app/immsnv_cor_profile.R
+++ gsca-r-app/immsnv_cor_profile.R
@@ -1,4 +1,4 @@
-########### immune cnv correlation ################
+########### immune methy correlation ################
 # Library -----------------------------------------------------------------
 
 library(magrittr)
@@ -14,7 +14,7 @@ search_str <- args[1]
 filepath <- args[2]
 apppath <- args[3]
 
-# search_str <- 'A2M#ACE#ANGPT2#BPI#CD1B#CDR1#EGR2#EGR3#HBEGF#HERPUD1#MCM2#PCTP#PODXL#PPY#PTGS2#RCAN1#SLC4A7#THBD@KICH_immune_cor_expr'
+# search_str <- 'A2M#ACE#ANGPT2#BPI#CD1B#CDR1#EGR2#EGR3#HBEGF#HERPUD1#MCM2#PCTP#PODXL#PPY#PTGS2#RCAN1#SLC4A7#THBD@KICH_immune_cor_snv'
 # apppath='/home/huff/github/GSCA'
 # filepath <- "/home/huff/github/GSCA/gsca-r-plot/pngs/3d2e17d3-91b9-40ff-bf8f-d9dd70692a26.png"
 
@@ -32,8 +32,8 @@ size <- fn_height_width(search_genes,search_cancertypes)
 # fetch data --------------------------------------------------------------
 
 source(file.path(apppath, "gsca-r-app/utils/fn_fetch_mongo_data.R"))
-fields <- '{"symbol": true, "cell_type": true,"cor": true,"fdr":true,"_id": false}'
-fetched_data <- purrr::map(.x = search_colls, .f = fn_fetch_mongo, pattern="_immune_cor_expr",fields = fields,.key=search_genes,.keyindex="symbol") %>%
+fields <- '{"symbol": true, "cell_type": true,"logfc": true,"fdr":true,"_id": false}'
+fetched_data <- purrr::map(.x = search_colls, .f = fn_fetch_mongo, pattern="_immune_cor_snv",fields = fields,.key=search_genes,.keyindex="symbol") %>%
   dplyr::bind_rows() %>%
   dplyr::mutate(logfdr = -log10(fdr)) %>%
   dplyr::mutate(logfdr = ifelse(logfdr>10,10,logfdr))
@@ -42,7 +42,7 @@ fetched_data <- purrr::map(.x = search_colls, .f = fn_fetch_mongo, pattern="_imm
 source(file.path(apppath,"gsca-r-app/utils/common_used_summary_plot_functions.R"))
 
 fetched_data %>%
-  dplyr::mutate(group = ifelse(logfdr>1.3,"<0.05",">0.05")) -> for_plot
+  dplyr::mutate(group = ifelse(logfdr>1.3,"FDR<0.05","FDR>0.05")) -> for_plot
 fetched_data_clean_pattern <- fn_get_pattern_celltype(.x = for_plot %>% 
                                                         dplyr::mutate(value=fdr) %>%
                                                         dplyr::mutate(trend=ifelse(cor>0,"Pos","Neg")),
@@ -56,29 +56,8 @@ gene_rank <- fn_get_gene_rank(.x = fetched_data_clean_pattern)
 # plot --------------------------------------------------------------------
 
 source(file.path(apppath,"gsca-r-app/utils/fn_bubble_plot_immune.R"))
-for_plot$cor %>% range() -> min_max
-trunc(min_max[1]*10)/10 -> min
-ceiling(min_max[2]*10)/10 -> max
-fillbreaks <- sort(unique(c(0,seq(min,max,by = 0.5))))
-plot <- bubble_plot(data=for_plot, 
-                    cancer="cell_type", 
-                    gene="symbol", 
-                    xlab="Cell type", 
-                    ylab="Symbol",
-                    facet_exp=NA,
-                    size="logfdr",
-                    fill="cor",
-                    fillmipoint =0,
-                    fillbreaks =fillbreaks,
-                    colorgroup="group",
-                    cancer_rank=celltype_rank$cell_type, 
-                    gene_rank=gene_rank$symbol, 
-                    sizename= "-Log10(FDR)", 
-                    colorvalue=c("black","grey"),
-                    colorbreaks=c("<0.05",">0.05"),
-                    colorname="FDR", 
-                    fillname="Correlation", 
-                    title="")
+
+plot <- bubble_plot(data=for_plot, cancer="cell_type", gene="symbol", xlab="Cell type", ylab="Symbol", size="logfdr", color="cor", colorgroup="group",cancer_rank=celltype_rank$cell_type, gene_rank=gene_rank$symbol, sizename= "-Log10(FDR)", colorname="Correlation", title="")
 
 # Save --------------------------------------------------------------------
 ggsave(filename = filepath, plot = plot, device = 'png', width = size$width, height = size$height+2)
