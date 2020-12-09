7df6e3ae222a9144115bee5f40d664d383cc84fa
Huffyphenix <949985347@qq.com>
2020-12-09 09:33:13 +0800

    R cnv survival optimize

--- gsca-r-app/cnv_survivalplot_profile.R
+++ gsca-r-app/cnv_survivalplot_profile.R
@@ -13,7 +13,7 @@ args <- commandArgs(TRUE)
 search_str <- args[1]
 filepath <- args[2]
 apppath <- args[3]
-# search_str <- 'A2M#ACE#ANGPT2#BPI#CD1B#CDR1#EGR2#EGR3#HBEGF#HERPUD1#MCM2#PCTP#PODXL#PPY#PTGS2#RCAN1#SLC4A7#THBD@KICH_cnv_survival#KIRC_cnv_survivalKIRP_cnv_survival#LUAD_cnv_survival#LUSC_cnv_survival'
+# search_str <- 'A2M#ACE#ANGPT2#CBX2#PDL1#EZH2#BPI#CD1B#CDR1#EGR2#EGR3#HBEGF#HERPUD1#MCM2#PCTP#PODXL#PPY#PTGS2#RCAN1#SLC4A7#THBD@KICH_cnv_survival#KIRC_cnv_survival#KIRP_cnv_survival#LUAD_cnv_survival#LUSC_cnv_survival'
 # apppath='/home/huff/github/GSCA'
 
 search_str_split <- strsplit(x = search_str, split = '@')[[1]]
@@ -45,28 +45,41 @@ fetched_data_clean_pattern <- fn_get_pattern(
   selections =c("cancertype","symbol"))
 cancer_rank <- fn_get_cancer_types_rank(.x = fetched_data_clean_pattern)
 gene_rank <- fn_get_gene_rank(.x = fetched_data_clean_pattern)
-for_plot <- fn_pval_label(.x = fetched_data %>% dplyr::rename(value=log_rank_p)) %>%
+for_plot <- fetched_data %>% 
+  dplyr::rename(value=log_rank_p) %>%
   dplyr::mutate(logp=ifelse(value==0,10,-log10(value))) %>%
-  dplyr::mutate(logp=ifelse(logp>10,10,logp))
+  dplyr::mutate(logp=ifelse(logp>10,10,logp)) %>%
+  dplyr::mutate(group=ifelse(value>0.05,">0.05","<0.05"))
 
 # Plot --------------------------------------------------------------------
-source(file.path(apppath,"gsca-r-app/utils/fn_survival_summary_plot.R"))
+source(file.path(apppath,"gsca-r-app/utils/fn_bubble_plot_immune.R"))
 CPCOLS <- c("blue", "white", "red")
+for_plot %>%
+  dplyr::filter(!is.na(logp)) %>%
+  .$logp -> logp_value
+min(logp_value) %>% trunc() -> min
+max(logp_value) %>% ceiling() -> max
+fillbreaks <- sort(unique(c(1.3,seq(min,max,length.out = 3))))
 
-heat_plot <- fn_survival_summary_plot_onlyP(data = for_plot,
-                                            aesx = "cancertype", 
-                                            aesy = "symbol",
-                                            fill = "logp",
-                                            fill_low=CPCOLS[1],
-                                            fill_high=CPCOLS[3],
-                                            fill_mid=CPCOLS[2],
-                                            fill_midpoint=1.3,
-                                            y_rank = gene_rank$symbol,
-                                            x_rank = cancer_rank$cancertype,
-                                            min=0,
-                                            max=10,
-                                            fill_name = "-Log(10) P",title = "",xlab = "Cancer types",ylab = "Symbol")
-
+heat_plot <- bubble_plot(data=for_plot, 
+                         cancer="cancertype", 
+                         gene="symbol", 
+                         xlab="Cancer types", 
+                         ylab="Symbol", 
+                         facet_exp = ".~sur_type",
+                         size="logp", 
+                         fill="logp", 
+                         fillmipoint =1.3,
+                         fillbreaks =fillbreaks,
+                         colorgroup="group",
+                         cancer_rank=cancer_rank$cancertype, 
+                         gene_rank=gene_rank$symbol, 
+                         sizename= "-Log(10) P", 
+                         fillname="-Log(10) P", 
+                         colorvalue=c("black","grey"), 
+                         colorbreaks=c("<0.05",">0.05"),
+                         colorname="Logrank P",
+                         title="")
 # Save --------------------------------------------------------------------
 ggsave(filename = filepath, plot = heat_plot, device = 'png', width = size$width, height = size$height)
 pdf_name <- gsub("\\.png",".pdf",filepath)
