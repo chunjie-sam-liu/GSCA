2a98a536bfe17db5830153f55ca2baab7b1dd7a8
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-12-10 16:36:07 +0800

    gene set

--- gsca-angular/src/app/modules/expression/gene-set/gene-set.component.ts
+++ gsca-angular/src/app/modules/expression/gene-set/gene-set.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, AfterViewInit, SimpleChanges, Input } from '@angular/core';
+import { ExpressionApiService } from './../expression-api.service';
+import { ExprSearch } from './../../../shared/model/exprsearch';
+import collectionlist from 'src/app/shared/constants/collectionlist';
 
 @Component({
   selector: 'app-gene-set',
   templateUrl: './gene-set.component.html',
-  styleUrls: ['./gene-set.component.css']
+  styleUrls: ['./gene-set.component.css'],
 })
-export class GeneSetComponent implements OnInit {
+export class GeneSetComponent implements OnInit, OnChanges, AfterViewInit {
+  @Input() searchTerm: ExprSearch;
 
-  constructor() { }
+  constructor(private expressionApiService: ExpressionApiService) {}
 
   ngOnInit(): void {
+    console.error(this.searchTerm);
   }
 
+  ngOnChanges(changes: SimpleChanges): void {}
+
+  ngAfterViewInit(): void {}
+
+  private _validCollection(st: ExprSearch): any {
+    return st;
+  }
 }
