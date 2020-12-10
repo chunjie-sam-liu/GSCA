4a1f3798ac88a98f3ec13176abb8b51778ad8dce
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-12-10 14:53:00 +0800

    remove echarts and pdfjs

--- gsca-angular/src/app/shared/shared.module.ts
+++ gsca-angular/src/app/shared/shared.module.ts
@@ -1,25 +1,33 @@
-import { PdfModule } from './pdf.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialElevationDirective } from './directives/material-elevation.directive';
 import { MaterialModule } from './material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { EchartsModule } from './echarts.module';
+// import { EchartsModule } from './echarts.module';
+// import { PdfModule } from './pdf.module';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [MaterialElevationDirective, HeaderComponent, FooterComponent, SidebarComponent],
-  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, MaterialModule, EchartsModule, PdfModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    // EchartsModule,
+    // PdfModule
+  ],
   exports: [
     MaterialElevationDirective,
     FormsModule,
     ReactiveFormsModule,
     MaterialModule,
-    EchartsModule,
-    PdfModule,
+    // EchartsModule,
+    // PdfModule,
     HeaderComponent,
     FooterComponent,
     SidebarComponent,
