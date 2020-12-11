c7329727a957064ffe1c0ca43682a74b43a280bb
Huffyphenix <949985347@qq.com>
2020-12-11 16:50:27 +0800

    generate gdsc comp

--- gsca-angular/src/app/modules/drug/drug.module.ts
+++ gsca-angular/src/app/modules/drug/drug.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { DrugRoutingModule } from './drug-routing.module';
 import { DrugComponent } from './drug.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { GdscComponent } from './gdsc/gdsc.component';
 
 @NgModule({
-  declarations: [DrugComponent],
+  declarations: [DrugComponent, GdscComponent],
   imports: [CommonModule, DrugRoutingModule, SharedModule],
 })
 export class DrugModule {}
---
+++ gsca-angular/src/app/modules/drug/gdsc/gdsc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gdsc',
+  templateUrl: './gdsc.component.html',
+  styleUrls: ['./gdsc.component.css']
+})
+export class GdscComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
