cbdaee20f210107ccfc735ce0f82f3723f4403e0
Huffyphenix <949985347@qq.com>
2020-12-11 16:09:13 +0800

    rscprit immune snv summary

--- gsca-r-app/immsnv_cor_profile.R
+++ gsca-r-app/immsnv_cor_profile.R
@@ -14,7 +14,7 @@ search_str <- args[1]
 filepath <- args[2]
 apppath <- args[3]
 
-# search_str <- 'A2M#ACE#ANGPT2#BPI#CD1B#CDR1#EGR2#EGR3#HBEGF#HERPUD1#MCM2#PCTP#PODXL#PPY#PTGS2#RCAN1#SLC4A7#THBD@KICH_immune_cor_snv'
+# search_str <- 'A2M#ACE#ANGPT2#BPI#CD1B#CDR1#EGR2#EGR3#HBEGF#HERPUD1#MCM2#PCTP#PODXL#PPY#PTGS2#RCAN1#SLC4A7#THBD@KIRC_immune_cor_snv'
 # apppath='/home/huff/github/GSCA'
 # filepath <- "/home/huff/github/GSCA/gsca-r-plot/pngs/3d2e17d3-91b9-40ff-bf8f-d9dd70692a26.png"
 
@@ -24,10 +24,7 @@ search_colls <- strsplit(x = search_str_split[[2]], split = '#')[[1]]
 search_cancertypes <- list(strsplit(x = search_str_split[[2]], split = '#')[[1]] )%>%
   purrr::pmap(.f=function(.x){strsplit(x = .x, split = '_')[[1]][1]}) %>% unlist()
 
-# pic size ----------------------------------------------------------------
 
-source(file.path(apppath, "gsca-r-app/utils/fn_figure_height.R"))
-size <- fn_height_width(search_genes,search_cancertypes)
 
 # fetch data --------------------------------------------------------------
 
@@ -38,14 +35,19 @@ fetched_data <- purrr::map(.x = search_colls, .f = fn_fetch_mongo, pattern="_imm
   dplyr::mutate(logfdr = -log10(fdr)) %>%
   dplyr::mutate(logfdr = ifelse(logfdr>10,10,logfdr))
 
+# pic size ----------------------------------------------------------------
+
+source(file.path(apppath, "gsca-r-app/utils/fn_figure_height.R"))
+size <- fn_height_width(length(unique(fetched_data$symbol)),length(unique(fetched_data$cancertype)))
+
 # Sort ----------------------------------------------------------------
 source(file.path(apppath,"gsca-r-app/utils/common_used_summary_plot_functions.R"))
 
 fetched_data %>%
-  dplyr::mutate(group = ifelse(logfdr>1.3,"FDR<0.05","FDR>0.05")) -> for_plot
+  dplyr::mutate(group = ifelse(fdr<=0.05,"<0.05",">0.05")) -> for_plot
 fetched_data_clean_pattern <- fn_get_pattern_celltype(.x = for_plot %>% 
                                                         dplyr::mutate(value=fdr) %>%
-                                                        dplyr::mutate(trend=ifelse(cor>0,"Pos","Neg")),
+                                                        dplyr::mutate(trend=ifelse(logfc>0,"Pos","Neg")),
                                                       trend1="Pos",
                                                       trend2="Neg",
                                                       p_cutoff=0.05,
@@ -56,8 +58,31 @@ gene_rank <- fn_get_gene_rank(.x = fetched_data_clean_pattern)
 # plot --------------------------------------------------------------------
 
 source(file.path(apppath,"gsca-r-app/utils/fn_bubble_plot_immune.R"))
-
-plot <- bubble_plot(data=for_plot, cancer="cell_type", gene="symbol", xlab="Cell type", ylab="Symbol", size="logfdr", color="cor", colorgroup="group",cancer_rank=celltype_rank$cell_type, gene_rank=gene_rank$symbol, sizename= "-Log10(FDR)", colorname="Correlation", title="")
+for_plot %>%
+  dplyr::filter(!is.na(logfc)) %>%
+  .$logfc %>% range() -> min_max
+trunc(min_max[1]) -> min
+trunc(min_max[2]) -> max
+fillbreaks <- sort(unique(c(0,min,max)))
+plot <- bubble_plot(data=for_plot, 
+                    cancer="cell_type", 
+                    gene="symbol", 
+                    xlab="Cell type", 
+                    ylab="Symbol",
+                    facet_exp=NA,
+                    size="logfdr",
+                    fill="logfc",
+                    fillmipoint =0,
+                    fillbreaks =fillbreaks,
+                    colorgroup="group",
+                    cancer_rank=celltype_rank$cell_type, 
+                    gene_rank=gene_rank$symbol, 
+                    sizename= "-Log10(FDR)", 
+                    colorvalue=c("black","grey"),
+                    colorbreaks=c("<0.05",">0.05"),
+                    colorname="FDR", 
+                    fillname="Log2(FC)", 
+                    title="")
 
 # Save --------------------------------------------------------------------
 ggsave(filename = filepath, plot = plot, device = 'png', width = size$width, height = size$height+2)
