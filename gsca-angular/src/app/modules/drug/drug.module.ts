3ef3d3a56de87cb70636bb3528d8454456afa23b
Huffyphenix <949985347@qq.com>
2020-12-11 16:52:52 +0800

    generate search box comp

--- gsca-angular/src/app/modules/drug/drug.module.ts
+++ gsca-angular/src/app/modules/drug/drug.module.ts
@@ -5,9 +5,10 @@ import { DrugRoutingModule } from './drug-routing.module';
 import { DrugComponent } from './drug.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { GdscComponent } from './gdsc/gdsc.component';
+import { SearchBoxComponent } from './search-box/search-box.component';
 
 @NgModule({
-  declarations: [DrugComponent, GdscComponent],
+  declarations: [DrugComponent, GdscComponent, SearchBoxComponent],
   imports: [CommonModule, DrugRoutingModule, SharedModule],
 })
 export class DrugModule {}
