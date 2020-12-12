80ebe6c93230e61d49eb2eb23ffa10f735f4b283
Huffyphenix <949985347@qq.com>
2020-12-12 10:18:06 +0800

    generate ctrp

---
+++ gsca-angular/src/app/modules/drug/ctrp/ctrp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ctrp',
+  templateUrl: './ctrp.component.html',
+  styleUrls: ['./ctrp.component.css']
+})
+export class CtrpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
