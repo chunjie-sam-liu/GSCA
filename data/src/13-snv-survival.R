140ae326ae0eefff8f23d98f12c5709785b1357f
Huffyphenix <949985347@qq.com>
2020-12-13 15:13:52 +0800

    rename mutation groups as mutant and WT

--- data/src/13-snv-survival.R
+++ data/src/13-snv-survival.R
@@ -18,7 +18,7 @@ file_list <- grep("*_survival.snv.rds.gz",dir(file.path(data_path,"cancer_snv_su
 snv_survival <- tibble::tibble()
 for (file in file_list) {
   .snv_survival <- readr::read_rds(file.path(data_path,"cancer_snv_survival_v1",file)) %>%
-    dplyr::mutate(cancer_types= strsplit(file,split = "_")[[1]][1])
+    dplyr::mutate(cancer_types= strsplit(file,split = "_")[[1]][1]) 
   if(nrow(snv_survival)<1){
     snv_survival<-.snv_survival
   } else {
@@ -44,12 +44,16 @@ fn_snv_survival_mongo <-function(cancer_types,data){
   
   message(glue::glue('Save all {.y} SNV survival into mongo'))
   
-  .xs
+  .x
 }
 
 # data --------------------------------------------------------------------
+groups <- tibble::tibble(higher_risk_of_death=c("Mutated","Non-mutated"),
+               higher_risk_of_death_rename = c("Mutant","WT"))
 snv_survival %>%
   dplyr::filter(!is.na(higher_risk_of_death)) %>%
+  dplyr::inner_join(groups, by="higher_risk_of_death") %>%
+  dplyr::select(-higher_risk_of_death,higher_risk_of_death=higher_risk_of_death_rename) %>%
   tidyr::nest(data = c(Hugo_Symbol, entrez, logrankp, cox_p, hr, higher_risk_of_death,sur_type)) %>%
   purrr::pmap(.f=fn_snv_survival_mongo) -> snv_survival_mongo_data
 
