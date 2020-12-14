e4f724b43c20c9484b739277148704c22520647d
Huffyphenix <949985347@qq.com>
2020-12-14 23:40:37 +0800

    add more stage plot

--- gsca/routes/expression/stage.py
+++ gsca/routes/expression/stage.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, send_file
 from gsca.db import mongo
 from flask_restful import Api, Resource, fields, marshal_with, reqparse
-from gsca.utils.checkplot import CheckPlot
+from gsca.utils.checkplot import CheckPlot, CheckParallelPlot
 
 stage = Blueprint("stage", __name__)
 api = Api(stage)
@@ -41,13 +41,16 @@ api.add_resource(StageTable, "/stagetable")
 class StagePlot(Resource):
     def post(self):
         args = request.get_json()
-        checkplot = CheckPlot(args=args, purpose="stageplot", rplot="exp_stageplot_profile.R")
-        res = checkplot.check_run()
+        purposes = ("stagePoint", "stageHeat", "stageTrend")
+        checkplot = CheckParallelPlot(args=args, purposes=purposes, rplot="exp_stageplot_profile.R")
+        check_run = checkplot.check_run()
 
-        if res["run"]:
-            checkplot.plot(filepath=res["filepath"])
+        if any([res["run"] for res in check_run.values()]):
+            filepaths = [str(check_run[purpose]["filepath"]) for purpose in purposes]
+            checkplot.plot(filepaths=filepaths)
         # return send_file(str(res["filepath"]), mimetype="image/png")
-        return {"stageplotuuid": res["uuid"]}
+        uuidnames = {purpose + "uuid": check_run[purpose]["uuid"] for purpose in purposes}
+        return uuidnames
 
 
 api.add_resource(StagePlot, "/stageplot")
