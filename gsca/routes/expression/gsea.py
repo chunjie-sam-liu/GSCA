479ca7d4f814ee2055850d50efd287cb576c007f
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-12-15 11:06:51 +0800

    gsea done

--- gsca/routes/expression/gsea.py
+++ gsca/routes/expression/gsea.py
@@ -1,3 +1,4 @@
+from gsca.utils.checkplot import CheckUUIDPlot
 from gsca.utils.checktable import CheckTableGSXA
 from flask import Blueprint, request
 from flask_restful import Api, Resource
@@ -21,3 +22,22 @@ class GSEAAnalysis(Resource):
 
 
 api.add_resource(GSEAAnalysis, "/gseaanalysis")
+
+
+class ExprGSEAPlot(Resource):
+    def get(self, uuidname):
+        checkplot = CheckUUIDPlot(
+            gsxa_uuid=uuidname,
+            name_uuid="gsea_uuid",
+            purpose="exprgseaplot",
+            rplot="expr_gsea_plot.R",
+            precol="preanalysised",
+            gsxacol="preanalysised_gsea",
+        )
+        res = checkplot.check_run()
+        if res["run"]:
+            checkplot.plot()
+        return {"exprgseaplotuuid": res["uuid"], "exprgseatableuuid": uuidname}
+
+
+api.add_resource(ExprGSEAPlot, "/exprgseaplot/<string:uuidname>")
