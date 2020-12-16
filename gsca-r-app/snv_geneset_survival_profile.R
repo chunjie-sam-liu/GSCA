46b862b0f7d0f60cbfb36e59c210e4685bf0b9bc
Huffyphenix <949985347@qq.com>
2020-12-16 18:32:22 +0800

    rscript geneset snv survival plot

--- gsca-r-app/snv_geneset_survival_profile.R
+++ gsca-r-app/snv_geneset_survival_profile.R
@@ -8,33 +8,91 @@ library(dplyr)
 
 args <- commandArgs(TRUE)
 
-search_str <- args[1]
-filepath <- args[2]
-apppath <- args[3]
-# search_str <- 'A2M#ACE#ANGPT2#BPI#CD1B#CDR1#EGR2#EGR3#HBEGF#HERPUD1#MCM2#PCTP#PODXL#PPY#PTGS2#RCAN1#SLC4A7#THBD@KICH_snv_survival#KIRC_snv_survival#KIRP_snv_survival#LUAD_snv_survival#LUSC_snv_survival'
+tableuuid <- args[1]
+tablecol <- args[2]
+filepath <- args[3]
+apppath <- args[4]
+
+# tableuuid <- 'd6fe01ae-b1fe-4f9b-ac92-60210edca6bc'
+# tablecol <- 'preanalysised_snvgeneset'
+# filepath <- "/home/liucj/github/GSCA/gsca-r-plot/pngs/688682d4-977f-432b-8338-f0c28730cbcb.png"
 # apppath <- '/home/huff/github/GSCA'
-# filepath <- '/home/huff/github/GSCA/gsca-r-plot/pngs/0b7ebc57-c2bd-4fd0-8775-5458e514cdb1.png'
 
-search_str_split <- strsplit(x = search_str, split = '@')[[1]]
-search_genes <- strsplit(x = search_str_split[1], split = '#')[[1]]
-# search_cancertypes <- strsplit(x = search_str_split[[2]], split = '#')[[1]]
-search_colls <- strsplit(x = search_str_split[[2]], split = '#')[[1]]
-search_cancertypes <- list(strsplit(x = search_str_split[[2]], split = '#')[[1]] )%>%
-  purrr::pmap(.f=function(.x){strsplit(x = .x, split = '_')[[1]][1]}) %>% unlist()
+# Mongo -------------------------------------------------------------------
+
+source(file.path(apppath, "gsca-r-app/utils/fn_fetch_mongo_data.R"))
+source(file.path(apppath, "gsca-r-app/utils/fn_geneset_survival.R"))
+pre_gsva_coll <- mongolite::mongo(collection = tablecol, url = gsca_conf)
+post_gsva_coll <- mongolite::mongo(collection = glue::glue("{tablecol}_survival"), url = gsca_conf)
+
+# Function ----------------------------------------------------------------
 
 
-# load data ---------------------------------------------------------------
-source(file.path(apppath,"gsca-r-app/snv_survival_geneset.R"))
+fn_query_str <- function(.x) {
+  .xx <- paste0(.x, collapse = '","')
+  glue::glue('{"uuid": "<.xx>"}', .open = '<', .close = '>')
+}
 
-# geneset_survival <-  readr::read_tsv(file.path(apppath,"gsca-r-plot/tables","geneset_survival_table.tsv"))
+fn_fetch_data <- function(.uuid) {
+  pre_gsva_coll$find(query = fn_query_str(.x = .uuid), fields = '{"_id": false}')
+}
+
+# fetch data --------------------------------------------------------------
+
+fetched_data <- fn_fetch_data(.uuid = tableuuid)$snvgeneset[[1]] %>%
+  dplyr::mutate(sample_name=substr(barcode,1,12)) %>%
+  tidyr::nest(data = c(barcode, group , sample_name))
+
+fields <- '{"cancer_types": true, "sample_name": true, "os_days": true,"os_status": true, "pfs_days": true,"pfs_status": true,"_id": false}'
+fetched_survival_data <- purrr::map(.x = "all_survival", .f = fn_fetch_mongo, pattern="_survival",fields = fields,.key=fetched_data$cancertype,.keyindex="cancer_types") %>%
+  dplyr::bind_rows() %>%
+  dplyr::group_by(cancer_types) %>%
+  tidyr::nest() %>%
+  dplyr::rename(cancertype=cancer_types)
+
+
+# combine data ------------------------------------------------------------
+
+fetched_data %>%
+  dplyr::mutate(combine = purrr::map2(cancertype,data,.f=function(.x,.y){
+    fetched_survival_data %>%
+      dplyr::filter(cancertype %in% .x) %>%
+      tidyr::unnest() %>%
+      dplyr::inner_join(.y,by=c("sample_name")) %>%
+      dplyr::mutate(group=ifelse(is.na(group),"1WT",group))
+  })) %>%
+  dplyr::ungroup() %>%
+  dplyr::select(-data) -> combine_data_group
+
+# calculation -------------------------------------------------------------
+
+combine_data_group %>%
+  dplyr::mutate(surviva_res = purrr::map2(cancertype,combine,fn_survival_res)) %>%
+  dplyr::select(-combine) %>%
+  tidyr::unnest(cols = c(surviva_res)) %>%
+  dplyr::filter(!is.na(higher_risk_of_death))-> geneset_survival
+
+# Insert table ------------------------------------------------------------
+insert_data <- list(uuid = tableuuid, res_table = geneset_survival)
+#post_gsva_coll$drop()
+uuid_query <- post_gsva_coll$find(
+  query = fn_query_str(.x = tableuuid),
+  fields = '{"uuid": true, "_id": false}'
+)
+
+if (nrow(uuid_query) == 0) {
+  post_gsva_coll$insert(data = insert_data)
+  post_gsva_coll$index(add = '{"uuid": 1}')
+  message("insert data into preanalysised_snvgeneset_survival")
+}
 
 # pic size ----------------------------------------------------------------
 
 source(file.path(apppath, "gsca-r-app/utils/fn_figure_height.R"))
-size <- fn_height_width(search_genes,search_cancertypes)
+size <- fn_height_width(unique(geneset_survival$cancertype),unique(geneset_survival$sur_type))
 
 color_list <- tibble::tibble(color=c( "#CD2626","#00B2EE"),
-                             group=c("Mutated","Non-mutated"))
+                             group=c("Mutant","WT"))
 
 # rank --------------------------------------------------------------------
 source(file.path(apppath,"gsca-r-app/utils/common_used_summary_plot_functions.R"))
--- gsca-r-app/utils/fn_geneset_snv.R
+++ gsca-r-app/utils/fn_geneset_snv.R
@@ -3,8 +3,8 @@
 ####################################
 fn_geneset_snv <- function(.x){
   .x %>%
-    dplyr::select(symbol,sample_name,group) %>%
-    dplyr::group_by(sample_name) %>%
+    dplyr::select(symbol,barcode,group) %>%
+    dplyr::group_by(barcode) %>%
     tidyr::nest() %>%
     dplyr::mutate(group = purrr::map(data,.f=function(.y){
       mutated_n <- length(grep("2Mutant",.y$group,value = T))
