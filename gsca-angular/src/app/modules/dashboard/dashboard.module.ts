4af2703320a2aceec59312b871787b29bf0c757c
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-12-17 11:22:37 +0800

    for citation

--- gsca-angular/src/app/modules/dashboard/dashboard.module.ts
+++ gsca-angular/src/app/modules/dashboard/dashboard.module.ts
@@ -6,9 +6,10 @@ import { DashboardComponent } from './dashboard.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { SearchBoxComponent } from './search-box/search-box.component';
 import { FeatureDiscriptionComponent } from './feature-discription/feature-discription.component';
+import { CitationComponent } from './citation/citation.component';
 
 @NgModule({
-  declarations: [DashboardComponent, SearchBoxComponent, FeatureDiscriptionComponent],
+  declarations: [DashboardComponent, SearchBoxComponent, FeatureDiscriptionComponent, CitationComponent],
   imports: [CommonModule, DashboardRoutingModule, SharedModule],
 })
 export class DashboardModule {}
