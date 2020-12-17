127ea2c642872da08df4f5297fdd38b94a15daeb
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-12-17 10:59:06 +0800

    features

--- gsca-angular/src/app/modules/dashboard/feature-discription/feature-discription.component.ts
+++ gsca-angular/src/app/modules/dashboard/feature-discription/feature-discription.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-feature-discription',
   templateUrl: './feature-discription.component.html',
-  styleUrls: ['./feature-discription.component.css']
+  styleUrls: ['./feature-discription.component.css'],
 })
 export class FeatureDiscriptionComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
