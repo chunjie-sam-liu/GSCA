941ade469a3905b1c874b043c52c602204654e1b
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-12-17 09:42:30 +0800

    search box

--- gsca-angular/src/app/modules/dashboard/search-box/search-box.component.ts
+++ gsca-angular/src/app/modules/dashboard/search-box/search-box.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-search-box',
   templateUrl: './search-box.component.html',
-  styleUrls: ['./search-box.component.css']
+  styleUrls: ['./search-box.component.css'],
 })
 export class SearchBoxComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
