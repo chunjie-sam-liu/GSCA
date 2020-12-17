e92aae5c8a21f4b671dc52538b69a010c69a7599
Huffyphenix <949985347@qq.com>
2020-12-17 15:17:27 +0800

    document v1

--- gsca-angular/src/app/modules/document/document.component.ts
+++ gsca-angular/src/app/modules/document/document.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-document',
   templateUrl: './document.component.html',
-  styleUrls: ['./document.component.css']
+  styleUrls: ['./document.component.css'],
 })
 export class DocumentComponent implements OnInit {
+  public assets = environment.assets;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
