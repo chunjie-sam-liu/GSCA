edbc2df7a536a5ec0e414bf0b6534877d4149885
Huffyphenix <949985347@qq.com>
2020-12-18 23:49:44 +0800

    doc cancer stat table

--- gsca-angular/src/app/modules/document/document.component.ts
+++ gsca-angular/src/app/modules/document/document.component.ts
@@ -1,9 +1,11 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import immunecellstable from 'src/app/shared/constants/immunecells';
+import cancerstat from 'src/app/shared/constants/cancerstatistical';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { ImmCellTableRecord } from 'src/app/shared/model/immcelltablerecord';
+import { CancerStatTableRecord } from 'src/app/shared/model/cancerstattablerecord';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatAccordion } from '@angular/material/expansion';
 
@@ -20,58 +22,42 @@ export class DocumentComponent implements OnInit, AfterViewInit {
   @ViewChild('accordion', { static: true }) Accordion: MatAccordion;
   accordionList: any;
 
+  // immune cells
   public immunecells = new MatTableDataSource<ImmCellTableRecord>(immunecellstable);
   @ViewChild('paginatorImm') paginatorImm: MatPaginator;
   @ViewChild(MatSort) sortImm: MatSort;
-  public immtab = immunecellstable;
 
   displayedColumnsImmuneCells = ['Immuneabbreviation', 'fullname'];
   displayedColumnsImmuneCellsHeader = ['Immune cells abbreviation', 'Immune cells fullname'];
-  constructor() {
-    this.accordionList = [
-      {
-        id: 'panel-1',
-        title: 'Expression module',
-        description: '',
-        isDisabled: false,
-        isExpanded: false,
-      },
-      {
-        id: 'panel-2',
-        title: 'Immune module',
-        description: '',
-        isDisabled: true,
-        isExpanded: false,
-      },
-      {
-        id: 'panel-3',
-        title: 'Mutation module',
-        description: '',
-        isDisabled: false,
-        isExpanded: true,
-      },
-      {
-        id: 'panel-4',
-        title: 'Drug module',
-        description: '',
-        isDisabled: false,
-        isExpanded: true,
-      },
-    ];
-  }
+
+  // cancer statistical
+  public cancerstat = new MatTableDataSource<CancerStatTableRecord>(cancerstat);
+  @ViewChild('paginatorCan') paginatorCan: MatPaginator;
+  @ViewChild(MatSort) sortCan: MatSort;
+
+  displayedColumnsCan = ['cancer_types', 'expr', 'survival', 'stage', 'immune', 'cnv', 'snv', 'methy'];
+  displayedColumnsCanHeader = ['Cancer types', 'mRNA expression', 'Survival', 'Stage', 'Immune', 'CNV', 'SNV', 'Methylation'];
+
+  constructor() {}
 
   ngOnInit(): void {}
 
   ngAfterViewInit() {
     this.immunecells.paginator = this.paginatorImm;
     this.immunecells.sort = this.sortImm;
+    this.cancerstat.paginator = this.paginatorCan;
+    this.cancerstat.sort = this.sortCan;
   }
   public applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.immunecells.filter = filterValue.trim().toLowerCase();
+    this.cancerstat.filter = filterValue.trim().toLowerCase();
 
     if (this.immunecells.paginator) {
       this.immunecells.paginator.firstPage();
+    }
+    if (this.cancerstat.paginator) {
+      this.cancerstat.paginator.firstPage();
     }
   }
   public beforePanelClosed(panel) {
