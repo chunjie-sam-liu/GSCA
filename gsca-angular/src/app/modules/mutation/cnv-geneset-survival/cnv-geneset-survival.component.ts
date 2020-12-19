4ad98d76954fae79bdeec763ea219c93151ef7e6
Huffyphenix <949985347@qq.com>
2020-12-19 10:33:51 +0800

    cnv geneset survival

---
+++ gsca-angular/src/app/modules/mutation/cnv-geneset-survival/cnv-geneset-survival.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cnv-geneset-survival',
+  templateUrl: './cnv-geneset-survival.component.html',
+  styleUrls: ['./cnv-geneset-survival.component.css']
+})
+export class CnvGenesetSurvivalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
