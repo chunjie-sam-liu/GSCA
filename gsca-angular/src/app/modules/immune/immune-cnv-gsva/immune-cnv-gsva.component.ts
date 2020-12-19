dac0b2f9e18265ec44efe58f5d9049f1708f0511
Huffyphenix <949985347@qq.com>
2020-12-19 14:00:19 +0800

    imm cnv gsva

---
+++ gsca-angular/src/app/modules/immune/immune-cnv-gsva/immune-cnv-gsva.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-immune-cnv-gsva',
+  templateUrl: './immune-cnv-gsva.component.html',
+  styleUrls: ['./immune-cnv-gsva.component.css']
+})
+export class ImmuneCnvGsvaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
