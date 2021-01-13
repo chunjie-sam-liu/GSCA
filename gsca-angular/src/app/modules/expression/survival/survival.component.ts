a60e1aeae0aa0c44b118a5d74825bbd18d7f088f
Huffyphenix <949985347@qq.com>
2021-01-13 16:44:25 +0800

    export survival table

--- gsca-angular/src/app/modules/expression/survival/survival.component.ts
+++ gsca-angular/src/app/modules/expression/survival/survival.component.ts
@@ -7,6 +7,7 @@ import { MatSort } from '@angular/material/sort';
 import collectionlist from 'src/app/shared/constants/collectionlist';
 import { ExpressionApiService } from '../expression-api.service';
 import { animate, state, style, transition, trigger } from '@angular/animations';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-survival',
@@ -207,5 +208,11 @@ export class SurvivalComponent implements OnInit, OnChanges, AfterViewInit {
 
   public triggerDetail(element: SurvivalTableRecord): string {
     return element === this.expandedElement ? 'expanded' : 'collapsed';
+  }
+  public exportExcel() {
+    const workSheet = XLSX.utils.json_to_sheet(this.dataSourceSurvival.data, { header: this.displayedColumnsSurvival });
+    const workBook: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workBook, workSheet, 'SheetName');
+    XLSX.writeFile(workBook, 'ExpressionAndSurvivalTable.xlsx');
   }
 }
