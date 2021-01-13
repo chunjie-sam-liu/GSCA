5bcefbc89f651104426163e190b0542569e501ab
Huffyphenix <949985347@qq.com>
2021-01-13 17:45:29 +0800

    export snv survival

--- gsca-angular/src/app/modules/mutation/snv-survival/snv-survival.component.ts
+++ gsca-angular/src/app/modules/mutation/snv-survival/snv-survival.component.ts
@@ -7,7 +7,7 @@ import { MatSort } from '@angular/material/sort';
 import collectionlist from 'src/app/shared/constants/collectionlist';
 import { MutationApiService } from '../mutation-api.service';
 import { animate, state, style, transition, trigger } from '@angular/animations';
-
+import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-snv-survival',
   templateUrl: './snv-survival.component.html',
@@ -193,5 +193,11 @@ export class SnvSurvivalComponent implements OnInit, OnChanges, AfterViewInit {
 
   public triggerDetail(element: SnvSurvivalTableRecord): string {
     return element === this.expandedElement ? 'expanded' : 'collapsed';
+  }
+  public exportExcel() {
+    const workSheet = XLSX.utils.json_to_sheet(this.dataSourceSnvSurvival.data, { header: this.displayedColumnsSnvSurvival });
+    const workBook: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workBook, workSheet, 'SheetName');
+    XLSX.writeFile(workBook, 'SnvAndSurvivalTable.xlsx');
   }
 }
