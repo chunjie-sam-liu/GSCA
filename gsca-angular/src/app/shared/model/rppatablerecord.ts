8e7ed54de361563854afffca7395ba543d11ae92
Huffyphenix <949985347@qq.com>
2021-04-20 17:18:27 +0800

    rppa table record

---
+++ gsca-angular/src/app/shared/model/rppatablerecord.ts
@@ -0,0 +1,10 @@
+// tslint:disable-next-line:no-empty-interface
+export interface RPPATableRecord {
+  cancertype: string;
+  entrez: number;
+  symbol: string;
+  pathway: string;
+  diff: number;
+  fdr: number;
+  class: string;
+}
