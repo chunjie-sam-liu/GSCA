10c741d619794b1397eb14ca05c3dd4d30bb5cf5
Huffyphenix <949985347@qq.com>
2021-05-31 18:39:27 +0800

    home img workflow

--- gsca-angular/src/app/modules/dashboard/dashboard.component.ts
+++ gsca-angular/src/app/modules/dashboard/dashboard.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  public assets = environment.assets;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
