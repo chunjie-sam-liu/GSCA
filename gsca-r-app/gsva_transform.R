ca93b2375987168e9ecffd80cf6769eb407b840c
Huffyphenix <949985347@qq.com>
2021-06-03 10:36:10 +0800

    gsva score transform

---
+++ gsca-r-app/gsva_transform.R
@@ -0,0 +1,73 @@
+
+# Library -----------------------------------------------------------------
+
+library(magrittr)
+library(ggplot2)
+
+
+# Arguments ---------------------------------------------------------------
+
+
+args <- commandArgs(TRUE)
+
+tableuuid <- args[1]
+tablecol <- args[2]
+filepath <- args[3]
+apppath <- args[4]
+
+
+# tableuuid <- 'ba16c786-93ca-421e-a426-7a361f4c3e7a'
+# tablecol <- 'preanalysised_gsva'
+# filepath <- "/home/liucj/github/GSCA/gsca-r-plot/pngs/688682d4-977f-432b-8338-f0c28730cbcb.png"
+# apppath <- '/home/huff/github/GSCA'
+
+# Mongo -------------------------------------------------------------------
+
+gsca_conf <- readr::read_lines(file = file.path(apppath, 'gsca-r-app/gsca.conf'))
+pre_gsea_coll <- mongolite::mongo(collection = tablecol, url = gsca_conf)
+post_gsea_coll <- mongolite::mongo(collection = glue::glue("{tablecol}_transform"), url = gsca_conf)
+# Function ----------------------------------------------------------------
+fn_query_str <- function(.x) {
+  .xx <- paste0(.x, collapse = '","')
+  glue::glue('{"uuid": "<.xx>"}', .open = '<', .close = '>')
+}
+
+fn_fetch_data <- function(.uuid) {
+  pre_gsva_coll$find(query = fn_query_str(.x = tableuuid), fields = '{"_id": false}')
+}
+
+fn_reorg <- function(.x) {
+  
+  .x %>%
+    tibble::as_tibble() %>%
+    tidyr::gather(key = "barcode", value = "gsva") %>%
+    tidyr::separate(col = "barcode", into = c("barcode", "type"), sep = "#") %>%
+    dplyr::mutate(type = factor(x = type, levels = c("tumor", "normal"))) ->
+    .xx
+  .xx
+}
+
+# Process -----------------------------------------------------------------
+
+fetched_data <- fn_fetch_data(.uuid = tableuuid)
+
+fetched_data$gsva_score %>% 
+  tibble::as_tibble() %>% 
+  tidyr::gather(key = "cancertype", value = "gsva") %>% 
+  dplyr::mutate(gsva = purrr::map(.x = gsva, .f = fn_reorg)) %>%
+  tidyr::unnest()->
+  gsva_score
+
+# Insert table ------------------------------------------------------------
+insert_data <- list(uuid = tableuuid, res_table = gsva_score)
+#post_gsva_coll$drop()
+uuid_query <- post_gsva_coll$find(
+  query = fn_query_str(.x = tableuuid),
+  fields = '{"uuid": true, "_id": false}'
+)
+
+if (nrow(uuid_query) == 0) {
+  post_gsva_coll$insert(data = insert_data)
+  post_gsva_coll$index(add = '{"uuid": 1}')
+  message("insert data into preanalysised_gsva_transform")
+}
