4d1de09293f99f9deafbe721d9afbef3f8ccd045
Huffyphenix <949985347@qq.com>
2021-06-07 19:57:19 +0800

    debug

--- gsca-r-app/snv_summary.R
+++ gsca-r-app/snv_summary.R
@@ -13,7 +13,7 @@ filepath_snvtitvplot <- args[4]
 apppath <- args[5]
 
 
-# search_str = "MCM2@KIRP_snv_count"
+# search_str = "A2M@KICH_snv_count"
 # filepath= "/home/huff/github/GSCA/gsca-r-plot/pngs/7aa843d9-6287-468e-9d52-47c44ea1fe21.png"
 # apppath='/home/huff/github/GSCA'
 
@@ -30,10 +30,7 @@ search_cancertypes <- list(strsplit(x = search_str_split[[2]], split = '#')[[1]]
 source(file.path(apppath, "gsca-r-app/utils/fn_fetch_mongo_data.R"))
 fields <- '{"_id": false}'
 fetched_snv_maf <- purrr::map(.x = paste(search_cancertypes,"_snv_maf",sep=""), .f = fn_fetch_mongo, pattern="_snv_maf",fields = fields,.key=search_genes,.keyindex="symbol") %>%
-  dplyr::bind_rows()%>%
-  dplyr::rename(Hugo_Symbol=symbol)
-fetched_snv_maf %>%
-  dplyr::select(cancertype,Tumor_Sample_Barcode) -> clincial_info
+  dplyr::bind_rows()
 
 
 # judgement ---------------------------------------------------------------
@@ -43,6 +40,10 @@ fetched_snv_maf %>%
   dplyr::filter(Variant_Classification %in% effective_mut) -> fetched_snv_maf.non_synonymous
 
 if(nrow(fetched_snv_maf.non_synonymous)>0){
+  fetched_snv_maf %>%
+    dplyr::rename(Hugo_Symbol=symbol) ->fetched_snv_maf
+  fetched_snv_maf %>%
+    dplyr::select(cancertype,Tumor_Sample_Barcode) -> clincial_info
   maf_project <- read.maf(maf=fetched_snv_maf,clinicalData=clincial_info)
   # draw plot ---------------------------------------------------------------
   # summary plot
