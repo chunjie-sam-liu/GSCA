a8ae0dafbb2c55ebb72ebf7357c3485f24d20625
Huffyphenix <949985347@qq.com>
2021-06-07 20:45:11 +0800

    accurate messages

--- gsca-r-app/snv_summary.R
+++ gsca-r-app/snv_summary.R
@@ -82,14 +82,11 @@ if(nrow(fetched_snv_maf)>0){
       dev.off()
     }else{
       source(file.path(apppath, "gsca-r-app/utils/fn_NA_notice_fig.R"))
-      fn_NA_notice_fig("Caution: \nOncoplot requires at-least two genes for plottng.") -> p
+      fn_NA_notice_fig("Caution: \nOncoplot requires at-least two genes\nfor plottng.") -> p
       # Save --------------------------------------------------------------------
       ggsave(filename = filepath_snvoncoplot, plot = p, device = 'png', width = 6, height = 4)
-      ggsave(filename = filepath_snvtitvplot, plot = p, device = 'png', width = 6, height = 4)
-      pdf_name_snvtitvplot <- gsub("\\.png",".pdf",filepath_snvtitvplot)
       pdf_name_snvoncoplot <- gsub("\\.png",".pdf",filepath_snvoncoplot)
       ggsave(filename = pdf_name_snvoncoplot, plot = p, device = 'pdf', width = 6, height = 4)
-      ggsave(filename = pdf_name_snvtitvplot, plot = p, device = 'pdf', width = 6, height = 4)
     }
   }else{
     source(file.path(apppath, "gsca-r-app/utils/fn_NA_notice_fig.R"))
