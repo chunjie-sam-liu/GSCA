5bee406599b7ef76726e6d55302daad3d87019a6
xieguiyan <932617527@qq.com>
2021-06-07 10:52:37 +0800

    show gsea fig in doc

--- gsca-angular/src/app/modules/expression/gsea/doc-sea/doc-sea.component.ts
+++ gsca-angular/src/app/modules/expression/gsea/doc-sea/doc-sea.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-doc-sea',
   templateUrl: './doc-sea.component.html',
-  styleUrls: ['./doc-sea.component.css']
+  styleUrls: ['./doc-sea.component.css'],
 })
 export class DocSeaComponent implements OnInit {
+  public assets = environment.assets;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
