da5c3c2f66acd268f26a909fbef4ad9a4facfb83
Huffyphenix <949985347@qq.com>
2021-06-07 20:13:12 +0800

    snv survival NA caution message

--- gsca-r-app/snv_survivalplot_profile.R
+++ gsca-r-app/snv_survivalplot_profile.R
@@ -34,57 +34,67 @@ fields <- '{"symbol": true, "log_rank_p": true,"cox_p": true,"sur_type": true,"h
 fetched_data <- purrr::map(.x = search_colls, .f = fn_fetch_mongo, pattern="_snv_survival",fields = fields,.key=search_genes,.keyindex="symbol") %>%
   dplyr::bind_rows()
 
-# Sort ----------------------------------------------------------------
-source(file.path(apppath,"gsca-r-app/utils/common_used_summary_plot_functions.R"))
+if(nrow(fetched_data)>0){
+  # Sort ----------------------------------------------------------------
+  source(file.path(apppath,"gsca-r-app/utils/common_used_summary_plot_functions.R"))
+  
+  fetched_data_clean_pattern <- fn_get_pattern(
+    .x = fetched_data %>% dplyr::rename(value=log_rank_p,trend=higher_risk_of_death) %>% dplyr::filter(sur_type=="OS"),
+    trend1="Mutated",
+    trend2="Non-mutated",
+    p_cutoff=0.05,
+    selections =c("cancertype","symbol"))
+  cancer_rank <- fn_get_cancer_types_rank(.x = fetched_data_clean_pattern)
+  gene_rank <- fn_get_gene_rank(.x = fetched_data_clean_pattern)
+  for_plot <- fn_pval_label(.x = fetched_data %>% dplyr::rename(value=log_rank_p)) %>%
+    dplyr::mutate(HR = ifelse(HR>10,10,HR)) %>%
+    dplyr::mutate(logp = -log10(value),
+                  group = ifelse(value>0.05,">0.05","<0.05"))
+  
+  # Plot --------------------------------------------------------------------
+  source(file.path(apppath,"gsca-r-app/utils/fn_bubble_plot_immune.R"))
+  CPCOLS <- c("blue", "white", "red")
+  for_plot %>%
+    dplyr::filter(!is.na(HR)) %>%
+    .$HR -> HR_value
+  min(HR_value) %>% trunc() -> min
+  max(HR_value) %>% ceiling() -> max
+  fillbreaks <- sort(unique(c(1,seq(min,max,length.out = 3))))
+  
+  title <- "Survival difference between mutant and wT"
+  
+  heat_plot <- bubble_plot(data=for_plot, 
+                           cancer="cancertype", 
+                           gene="symbol", 
+                           xlab="Cancer type", 
+                           ylab="Symbol", 
+                           facet_exp = ".~sur_type",
+                           size="logp", 
+                           fill="HR", 
+                           fillmipoint =1,
+                           fillbreaks =fillbreaks,
+                           colorgroup="group",
+                           cancer_rank=cancer_rank$cancertype, 
+                           gene_rank=gene_rank$symbol, 
+                           sizename= "-Log(P)", 
+                           fillname="Hazard ratio", 
+                           colorvalue=c("black","grey"), 
+                           colorbreaks=c("<0.05",">0.05"),
+                           colorname="Logrank P value",
+                           title=title)
+  
+  # Save --------------------------------------------------------------------
+  ggsave(filename = filepath, plot = heat_plot, device = 'png', width = size$width, height = size$height)
+  pdf_name <- gsub("\\.png",".pdf",filepath)
+  ggsave(filename = pdf_name, plot = heat_plot, device = 'pdf', width = size$width, height = size$height)
+}else{
+  source(file.path(apppath, "gsca-r-app/utils/fn_NA_notice_fig.R"))
+  fn_NA_notice_fig("Caution: \nsurvival analysis is not applicable for your search.\nPlease check if there are no mutations\n found in your search gene list?") -> p
+  # Save --------------------------------------------------------------------
+  ggsave(filename = filepath, plot = p, device = 'png', width = 6, height = 4)
+  pdf_name <- gsub("\\.png",".pdf",filepath)
+  ggsave(filename = pdf_name, plot = p, device = 'pdf', width = 6, height = 4)
+}
 
-fetched_data_clean_pattern <- fn_get_pattern(
-  .x = fetched_data %>% dplyr::rename(value=log_rank_p,trend=higher_risk_of_death) %>% dplyr::filter(sur_type=="OS"),
-  trend1="Mutated",
-  trend2="Non-mutated",
-  p_cutoff=0.05,
-  selections =c("cancertype","symbol"))
-cancer_rank <- fn_get_cancer_types_rank(.x = fetched_data_clean_pattern)
-gene_rank <- fn_get_gene_rank(.x = fetched_data_clean_pattern)
-for_plot <- fn_pval_label(.x = fetched_data %>% dplyr::rename(value=log_rank_p)) %>%
-  dplyr::mutate(HR = ifelse(HR>10,10,HR)) %>%
-  dplyr::mutate(logp = -log10(value),
-                group = ifelse(value>0.05,">0.05","<0.05"))
-
-# Plot --------------------------------------------------------------------
-source(file.path(apppath,"gsca-r-app/utils/fn_bubble_plot_immune.R"))
-CPCOLS <- c("blue", "white", "red")
-for_plot %>%
-  dplyr::filter(!is.na(HR)) %>%
-  .$HR -> HR_value
-min(HR_value) %>% trunc() -> min
-max(HR_value) %>% ceiling() -> max
-fillbreaks <- sort(unique(c(1,seq(min,max,length.out = 3))))
-
-title <- "Survival difference between mutant and wT"
-
-heat_plot <- bubble_plot(data=for_plot, 
-                         cancer="cancertype", 
-                         gene="symbol", 
-                         xlab="Cancer type", 
-                         ylab="Symbol", 
-                         facet_exp = ".~sur_type",
-                         size="logp", 
-                         fill="HR", 
-                         fillmipoint =1,
-                         fillbreaks =fillbreaks,
-                         colorgroup="group",
-                         cancer_rank=cancer_rank$cancertype, 
-                         gene_rank=gene_rank$symbol, 
-                         sizename= "-Log(P)", 
-                         fillname="Hazard ratio", 
-                         colorvalue=c("black","grey"), 
-                         colorbreaks=c("<0.05",">0.05"),
-                         colorname="Logrank P value",
-                         title=title)
-
-# Save --------------------------------------------------------------------
-ggsave(filename = filepath, plot = heat_plot, device = 'png', width = size$width, height = size$height)
-pdf_name <- gsub("\\.png",".pdf",filepath)
-ggsave(filename = pdf_name, plot = heat_plot, device = 'pdf', width = size$width, height = size$height)
 
 
