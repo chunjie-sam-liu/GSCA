84df0b48522e4ef060bcec6ce99d2153fc374132
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2021-07-14 15:00:05 +0800

    immune select all  cancer types

--- gsca-angular/src/app/modules/immune/search-box/search-box.component.ts
+++ gsca-angular/src/app/modules/immune/search-box/search-box.component.ts
@@ -38,6 +38,13 @@ export class SearchBoxComponent implements OnInit {
     this.showList.showContent = false;
   }
 
+  public selectAllCancers(): void {
+    const allCancerTypes = this.cancerTypeList.map((v) => {
+      return v.shortname;
+    });
+    this.cancerTypeSelected.patchValue(allCancerTypes);
+  }
+
   public submit(str: string): void {
     this.inputString = this._getSearchSymbol(str).join(', ');
 
