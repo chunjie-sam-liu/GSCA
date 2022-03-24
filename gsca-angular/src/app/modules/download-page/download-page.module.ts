0df49ff275452628ebcd02b86f5878e2d7978bc9
Huffyphenix <949985347@qq.com>
2022-03-24 16:01:19 +0800

    generate snv download page

--- gsca-angular/src/app/modules/download-page/download-page.module.ts
+++ gsca-angular/src/app/modules/download-page/download-page.module.ts
@@ -5,9 +5,10 @@ import { DownloadPageRoutingModule } from './download-page-routing.module';
 import { DownloadPageComponent } from './download-page.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { MrnaComponent } from './mrna/mrna.component';
+import { SnvComponent } from './snv/snv.component';
 
 @NgModule({
-  declarations: [DownloadPageComponent, MrnaComponent],
+  declarations: [DownloadPageComponent, MrnaComponent, SnvComponent],
   imports: [CommonModule, DownloadPageRoutingModule, SharedModule],
 })
 export class DownloadPageModule {}
