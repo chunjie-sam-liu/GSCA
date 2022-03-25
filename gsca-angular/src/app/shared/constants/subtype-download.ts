99dd63da0906bd323e8cbc6d30fe3f2b59c4de3c
Huffyphenix <949985347@qq.com>
2022-03-25 09:13:49 +0800

    download table record

---
+++ gsca-angular/src/app/shared/constants/subtype-download.ts
@@ -0,0 +1,68 @@
+export default [
+  {
+    Cancer_type: 'BLCA',
+    Sample_size: '215',
+    File_name: 'BLCA.subtype.tsv.gz',
+    download: 'Download',
+  },
+  {
+    Cancer_type: 'BRCA',
+    Sample_size: '837',
+    File_name: 'BRCA.subtype.tsv.gz',
+    download: 'Download',
+  },
+  {
+    Cancer_type: 'COAD',
+    Sample_size: '202',
+    File_name: 'COAD.subtype.tsv.gz',
+    download: 'Download',
+  },
+  {
+    Cancer_type: 'GBM',
+    Sample_size: '500',
+    File_name: 'GBM.subtype.tsv.gz',
+    download: 'Download',
+  },
+  {
+    Cancer_type: 'HNSC',
+    Sample_size: '92',
+    File_name: 'HNSC.subtype.tsv.gz',
+    download: 'Download',
+  },
+  {
+    Cancer_type: 'KIRC',
+    Sample_size: '417',
+    File_name: 'KIRC.subtype.tsv.gz',
+    download: 'Download',
+  },
+  {
+    Cancer_type: 'LUAD',
+    Sample_size: '230',
+    File_name: 'LUAD.subtype.tsv.gz',
+    download: 'Download',
+  },
+  {
+    Cancer_type: 'LUSC',
+    Sample_size: '178',
+    File_name: 'LUSC.subtype.tsv.gz',
+    download: 'Download',
+  },
+  {
+    Cancer_type: 'READ',
+    Sample_size: '68',
+    File_name: 'READ.subtype.tsv.gz',
+    download: 'Download',
+  },
+  {
+    Cancer_type: 'STAD',
+    Sample_size: '295',
+    File_name: 'STAD.subtype.tsv.gz',
+    download: 'Download',
+  },
+  {
+    Cancer_type: 'UCEC',
+    Sample_size: '232',
+    File_name: 'UCEC.subtype.tsv.gz',
+    download: 'Download',
+  },
+];
