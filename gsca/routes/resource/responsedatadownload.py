8b99b746e6f69216249346e84651e53a3c082fac
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2022-03-25 15:59:57 -0400

    set up download

--- gsca/routes/resource/responsedatadownload.py
+++ gsca/routes/resource/responsedatadownload.py
@@ -4,21 +4,21 @@ from flask_restful import Api, Resource
 from gsca import app
 from pathlib import Path
 
-responseplot = Blueprint("responseplot", __name__)
-api = Api(responseplot)
+responsedatadownload = Blueprint("responsedatadownload", __name__)
+api = Api(responsedatadownload)
 
 
-class ResponsePlot(Resource):
+class ResponseDataDownload(Resource):
     apppath = Path(app.root_path).parent  # notice apppath parent
-    resource_pngs = apppath / "gsca-r-plot/pngs"
+    resource_data = apppath / "gsca-download/expr"
 
-    def get(self, uuidname):
-        filepath = self.resource_pngs / uuidname
+    def get(self, filename):
+
+        filepath = self.resource_data / filename
         if filepath.exists():
             return send_file(str(filepath), as_attachment=True)
         else:
             return {"Error": "Resource not exists."}
 
 
-api.add_resource(ResponsePlot, "/<string:uuidname>")
-
+api.add_resource(ResponseDataDownload, "/<string:filename>")
