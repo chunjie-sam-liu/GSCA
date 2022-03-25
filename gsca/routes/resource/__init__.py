21ae56dcbfd9c6dd14bdd3760b18d5d6739a8431
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2022-03-25 15:58:32 -0400

    data download

--- gsca/routes/resource/__init__.py
+++ gsca/routes/resource/__init__.py
@@ -1,7 +1,9 @@
 from gsca import app
 from gsca.routes.resource.responseplot import responseplot
 from gsca.routes.resource.responsetable import responsetable
+from gsca.routes.resource.responsedatadownload import responsedatadownload
 
 
 app.register_blueprint(responseplot, url_prefix="/api/resource/responseplot")
 app.register_blueprint(responsetable, url_prefix="/api/resource/responsetable")
+app.register_blueprint(responsedatadownload, url_prefix="/api/resource/ResponseDataDownload")
