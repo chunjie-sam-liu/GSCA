485e6955a4aba8cbf02ae5be3c5a36dc0b40b8b6
Huffyphenix <949985347@qq.com>
2022-09-28 17:42:58 +0800

    typos

--- gsca-angular/src/app/modules/expression/gsea/gsea.component.ts
+++ gsca-angular/src/app/modules/expression/gsea/gsea.component.ts
@@ -60,7 +60,7 @@ export class GseaComponent implements OnInit, OnChanges, AfterViewInit {
       this.showGSEATable = false;
       this.showGSEAImage = false;
       window.alert(
-        'The GSEA enrichment analysis is based on cancer types with sufficient paired tumor-normal samples (>= 10). These cancer types including THCA, KIRP, BLCA, LIHC, HNSC, BRCA, LUAD, PRAD, ESCA, KICH, LUSC, KIRC, STAD and COAD. Or you can explore "GSVA score" section which provides GSVA score for single sample.'
+        'The "GSEA score", "Differential expression", and "Differential GSVA" are based on cancer types with sufficient paired tumor-normal samples (>= 10). These cancer types including THCA, KIRP, BLCA, LIHC, HNSC, BRCA, LUAD, PRAD, ESCA, KICH, LUSC, KIRC, STAD and COAD. Or you can explore "GSVA score" section or other sections, which provides gene expression analysis without a limitation on the paired tumor-normal sample size'
       );
     } else {
       this.expressionApiService.getGSEAAnalysis(postTerm).subscribe(
