adaccccc83a9fd2a19594916b22d25a7a98bd74d
Huffyphenix <949985347@qq.com>
2022-09-28 17:19:11 +0800

    typo

--- gsca-angular/src/app/modules/expression/search-box/search-box.component.ts
+++ gsca-angular/src/app/modules/expression/search-box/search-box.component.ts
@@ -56,11 +56,11 @@ export class SearchBoxComponent implements OnInit {
       return;
     }
     if (searchTerm.validSymbol.length < 2) {
-      window.alert('Please input at least two gene symbol');
+      window.alert('Please input at least 2 gene symbols');
       return;
     }
     if (searchTerm.validSymbol.length > 500) {
-      window.alert('Please input at most 500 gene symbol');
+      window.alert('Please input at most 500 gene symbols');
       return;
     }
     if (Object.values(this.showList).indexOf(true) < 0) {
