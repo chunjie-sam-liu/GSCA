8e6e0c38668767d2b791e5b1cce72da1172f281e
Huffyphenix <949985347@qq.com>
2023-04-18 09:47:52 +0800

    small text size of plot title

--- gsca-r-app/utils/fn_boxplot_single_gene_in_cancer.R
+++ gsca-r-app/utils/fn_boxplot_single_gene_in_cancer.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 
 box_plot_single_gene_multi_cancers <- function(data,aesx,aesy,color,color_name,facets,color_labels,color_values,title,xlab,ylab){
-  
+
   data %>%
     ggplot(aes_string(x = aesx, y = aesy, color = color)) +
     geom_boxplot(outlier.colour = NA) +
@@ -33,7 +33,7 @@ box_plot_single_gene_multi_cancers <- function(data,aesx,aesy,color,color_name,f
   return(p)
 }
 box_plot_single_gene_multi_cancers_facetgrid <- function(data,aesx,aesy,color,color_name,facets,color_labels,color_values,title,xlab,ylab){
-  
+
   data %>%
     ggplot(aes_string(x = aesx, y = aesy, color = color)) +
     geom_boxplot(outlier.colour = NA) +
@@ -83,7 +83,7 @@ box_plot_single_gene_single_cancer <- function(data,aesx,aesy,color,color_name,c
     .tmp <- .max*0.1*i+.max
     label.y <- c(label.y,.tmp)
   }
-  
+
   data %>%
     ggplot(aes_string(x = aesx, y = aesy, color = color)) +
     geom_boxplot(outlier.colour = NA) +
@@ -127,7 +127,7 @@ box_plot_single_gene_single_cancer_nocompare <- function(data,aesx,aesy,color,co
   data %>%
     dplyr::select(aesy) %>%
     min() ->.min
-  
+
   data %>%
     ggplot(aes_string(x = aesx, y = aesy, color = color)) +
     geom_boxplot(outlier.colour = NA) +
@@ -135,7 +135,7 @@ box_plot_single_gene_single_cancer_nocompare <- function(data,aesx,aesy,color,co
     scale_y_continuous(limits = c(.min,.max+0.1*.max)) +
     scale_color_manual(name = color_name, labels = color_labels, values = color_values)+
     labs(title = title, x = xlab, y = ylab)  +
-    annotate("text", 
+    annotate("text",
              x = 1, y = .max+0.05*.max, # x and y coordinates of the text
              label = paste("FDR =", format(signif(fdr,2), scientific = TRUE))) +
     #ggpubr::stat_compare_means(comparisons = comp_list, method = method,label = "p.signif",label.y=label.y, vjust = 10) +
@@ -147,7 +147,7 @@ box_plot_single_gene_single_cancer_nocompare <- function(data,aesx,aesy,color,co
         linetype = "dashed",
         size = 0.2
       ),
-      plot.title = element_text(size = 18, hjust = 0.5),
+      plot.title = element_text(size = 14, hjust = 0.5),
       axis.title = element_text(size = 14, color = "black"),
       axis.ticks = element_line(color = "black"),
       axis.text.x = element_text(size = 12, angle = xangle, hjust = hjust, vjust = vjust, colour = "black"),
